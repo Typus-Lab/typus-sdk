fe37b93ca36b1660c4b76d8f7390d685a9ad1cd9
leoOptifi <chun.w@alphabit.software>
2022-11-28 16:16:12 +0800

    hotfix

--- utils/getSubVault.ts
+++ utils/getSubVault.ts
@@ -15,34 +15,40 @@ export async function subVaultsV2(packageId: string, registry: string, provider:
 
     //vault
     let tmpObj1 = await provider.getObjectsOwnedByObject(registry)
-    let vault = tmpObj1[0].objectId
-    console.log("vault: " + vault)
+    console.log("vault num under registry:" + tmpObj1.length)
+    if (tmpObj1.length != 0) {
+        let vault = tmpObj1[0].objectId
+        console.log("vault: " + vault)
 
-    //table
-    let tmpObj2 = await provider.getObject(vault)
-    //@ts-ignore
-    let table = tmpObj2.details.data.fields.value.fields.sub_vaults.fields.id.id
-    console.log("table: " + table)
-
-    //sub vault (maker / rolling / no_rolling) id 
-    let tmpObj3 = await provider.getObjectsOwnedByObject(table)
-    let subVaultsId: string[] = [];
-    tmpObj3.map(e => {
-        subVaultsId.push(e.objectId as string)
-    }
-    )
-    console.log("subVaults: ")
-    //@ts-ignore
-    console.log(subVaultsId)
-
-    //sub vault data
-    const subVaultsMap = new Map();
-    //@ts-ignore
-    for (let e of subVaultsId) {
-        let tmpObj4 = await provider.getObject(e)
+        //table
+        let tmpObj2 = await provider.getObject(vault)
         //@ts-ignore
-        subVaultsMap.set(tmpObj4.details.data.fields.name, tmpObj4.details.data.fields)
+        let table = tmpObj2.details.data.fields.value.fields.sub_vaults.fields.id.id
+        console.log("table: " + table)
+
+        //sub vault (maker / rolling / no_rolling) id 
+        let tmpObj3 = await provider.getObjectsOwnedByObject(table)
+        let subVaultsId: string[] = [];
+        tmpObj3.map(e => {
+            subVaultsId.push(e.objectId as string)
+        }
+        )
+        console.log("subVaults: ")
+        //@ts-ignore
+        console.log(subVaultsId)
+
+        //sub vault data
+        const subVaultsMap = new Map();
+        //@ts-ignore
+        for (let e of subVaultsId) {
+            let tmpObj4 = await provider.getObject(e)
+            //@ts-ignore
+            subVaultsMap.set(tmpObj4.details.data.fields.name, tmpObj4.details.data.fields)
+        }
+        // console.log(subVaultsMap.get("rolling").value.fields.users_table)
+        return subVaultsMap
+    } else {
+        console.log("no vault under this registry: " + registry);
+        return null;
     }
-    // console.log(subVaultsMap.get("rolling").value.fields.users_table)
-    return subVaultsMap
 }
