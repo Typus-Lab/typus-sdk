558cd4c3321df242fc0c1595f4475d29a8544e54
leoOptifi <chun.w@alphabit.software>
2022-11-28 16:16:29 +0800

    add comment

--- utils/getMintTx.ts
+++ utils/getMintTx.ts
@@ -13,3 +13,7 @@ export async function getMintTx(packageId: string, registry: string, amount: num
     }
     return tx
 }
+
+/*
+sui client call --gas-budget 10000 --package $PACKAGE --module "token" --function "mint" --args $REGISTRY 10000
+*/
