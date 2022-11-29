01f34020a7666a3dc2e76feb434bbb43e4e0042b
leoOptifi <chun.w@alphabit.software>
2022-11-29 13:41:53 +0800

    getNewSharkFinVaultTx

---
+++ scripts/getNewSharkFinVaultTx.ts
@@ -0,0 +1,16 @@
+import { getNewSharkFinVaultTx } from "../utils/getNewSharkFinVaultTx"
+import { PACKAGE_ID, REGISTRY } from "../constants"
+(async () => {
+    console.log("test for getNewSharkFinVaultTx()")
+
+    //0x27b3674c685046f66cad1d5496d2967894fa5329: token package
+    let typeArgument = "0x27b3674c685046f66cad1d5496d2967894fa5329::token::USDC"
+    let expiration = 1671344789
+    let bullish = true
+    let lowBarrierPrice = 1
+    let highBarrierPrice = 10
+
+    //the payload to mint test coin
+    let newSharkFinVaultTx: any = await getNewSharkFinVaultTx(PACKAGE_ID, REGISTRY, typeArgument, expiration, bullish, lowBarrierPrice, highBarrierPrice);
+    console.log(newSharkFinVaultTx)
+})()
