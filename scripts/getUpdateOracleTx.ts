6232f03ab630350d7af213cdda92a8005e949e80
leoOptifi <chun.w@alphabit.software>
2022-12-01 14:14:29 +0800

    hotfix

--- scripts/getUpdateOracleTx.ts
+++ scripts/getUpdateOracleTx.ts
@@ -1,7 +1,7 @@
 import { getUpdateOracleTx } from "../utils/getUpdateOracleTx"
 import { TOKEN_PACKAGE } from "../constants"
 (async () => {
-    console.log("test for getNewOracleTx()")
+    console.log("test for getUpdateOracleTx()")
 
     let typeArgument = "0x2::sui::SUI";
     let oracle = "0x7158f68a06bc09123fcba1645f4c56ca866a8e90" //created from "new oracle"
