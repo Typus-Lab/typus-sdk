010b266c9e86e47f66ee9d5ec0a8a6156da9f462
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-05 12:49:24 +0800

    add asset constant

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
-export const TOKEN_PACKAGE = "0x27b3674c685046f66cad1d5496d2967894fa5329";
-export const TOKEN_REGISTRY = "0xfaa99efa7efa1ec0a265c898bf31dd7f10637f38";
+export const TOKEN_PACKAGE = "0xe879e96fb90a8559127091f1176a2d10bb67c6ff";
+export const TOKEN_REGISTRY = "0x5dce92f67eeda471369b734deac0d540b751cea6";//fake btc registry
 export const SHARKFIN_PACKAGE = "0x3085e089d748e09ae2a5859531ec3bc34b1e13b4";
 export const SHARKFIN_REGISTRY = "0xd27515bcf75daba9cff1da5b93bd4f08c781cea2";
 export const DOV_PACKAGE = "0x6fbe0bd487e9ca263f682f8d192750ef4ecce73d"
@@ -8,3 +8,11 @@ export const COVERED_CALL_REGISTRY = "0xb02452d0bbef3cbdc7840ea5bad27bcdd4031675
 export const COVERED_CALL_MANAGER = "0x8517b2dbaae56eeb84352e4869b289459c68965b"
 export const DEFAULT_TYPE_ARGUMENT = "0x2::sui::SUI";
 export const TEST_MNEMONIC = "tackle wheat jungle viable memory dwarf swift fold purpose cattle impose horn";
+export const TOKEN_NAME = ["BTC", "ETH", "SUI", "USDC", "USDT"]
+export const TOKEN_NAME_TO_MODULE = {
+    "BTC": "token_btc",
+    "ETH": "token_eth",
+    "SUI": "token_sui",
+    "USDC": "token_usdc",
+    "USDT": "token_usdt",
+}
