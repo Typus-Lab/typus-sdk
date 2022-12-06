319925af5a3f27de968343431a20148b1803b6dd
Prince <prince.gao@aloha.software>
2022-12-06 17:54:34 +0800

    simplified crypto utils for sealed bid auction

--- test/testCrypto.ts
+++ test/testCrypto.ts
@@ -1,6 +1,20 @@
-import { getBidHash } from "../utils/auction/sealedAuction"
+import { decryptBid, encryptBid, getBidHash } from "../utils/auction/sealedAuction"
 
 (async () => {
-    let hash = getBidHash(100, 2, 123456)
+
+    let price = BigInt(100)
+    let size = BigInt(2)
+    let secret = "my secret"
+   
+    // encrypt bid info
+    let encryptedBid = encryptBid(price, size, secret)
+    console.log("encryptedBid: ", encryptedBid)
+
+    // get bid hash
+    let hash = getBidHash(price, size, encryptedBid[0])
     console.log("hash: ", hash)
+
+    // decrypt bid
+    let bid = decryptBid(encryptedBid[1], "my secret")
+    console.log("bid: ", bid)
 })()
--- utils/auction/sealedAuction.ts
+++ utils/auction/sealedAuction.ts
@@ -1,34 +1,59 @@
-import CryptoJS from "crypto-js";
 import { BcsWriter } from '@mysten/bcs';
 import { sha3_256 } from "js-sha3"
-import { randomInt } from "crypto"
+import encryptpwd from "encrypt-with-password";
+import random  from "random-bigint"
 
-// Encrypt bid info
-export function encryptBid(price: number, size: number, secret: string): [number, string] {
-    let rdnNumber = randomInt(Number.MAX_SAFE_INTEGER)
-    let msg = constructBidInfo(price, size, rdnNumber)
-    let encryptedBid = CryptoJS.AES.encrypt(msg, secret).toString();
-    return [rdnNumber, encryptedBid]
+/**
+ * Encrypt bid info
+ * @param {bigint} price bid price
+ * @param {bigint} size bid size
+ * @param {string} secret secret to encrypt the bid info
+ * @returns the first element is the random number generated during bid encryption process,
+ * and the second is bid ciphertext
+ */
+export function encryptBid(price: bigint, size: bigint, secret: string): [bigint, string] {
+    let randomNumber = random(64)
+    let msg = constructBidInfo(price, size, randomNumber)
+    let res = encryptpwd.encrypt(msg.toString("hex"), secret)
+    return [randomNumber, res]
 }
 
-// Decrypt to bid info
-export function decryptBid(encryptMsg: string, secret: string): [number, number, number] {
-    let bytes = CryptoJS.AES.decrypt(encryptMsg, secret).toString();
-    let price = bytes.slice(0, bytes.length / 3)
-    let size = bytes.slice(bytes.length / 3, bytes.length / 3 * 2)
-    let blindingFactor = bytes.slice(bytes.length / 3 * 2)
-    return [parseInt(price), parseInt(size), parseInt(blindingFactor)]
+/**
+ * Decrypt to bid info
+ * @param {string} encryptedBid bid ciphertext
+ * @param {string} secret secret to decrypt the ciphertext to plain bid info
+ * @returns [bid price, bid size, blindingFactor]
+ */
+export function decryptBid(encryptedBid: string, secret: string): [BigInt, BigInt, BigInt] {
+    let res = encryptpwd.decrypt(encryptedBid, secret)
+    let buffer = Buffer.from(res, "hex")
+    let price = buffer.slice(0, buffer.length / 3).readBigUInt64LE(0)
+    let size = buffer.slice(buffer.length / 3, buffer.length / 3 * 2).readBigUInt64LE(0)
+    let blindingFactor = buffer.slice(buffer.length / 3 * 2).readBigUInt64LE(0)
+    return [price, size, blindingFactor]
 }
 
-// get hash of bid info  - should be same as onchain
-export function getBidHash(price: number, size: number, blindingFactor: number): any {
+/**
+ * Get hash of bid info  - should be same as onchain
+ * @param {bigint} price bid price
+ * @param {bigint} size bid size
+ * @param {bigint} blindingFactor a random number
+ * @returns {string} the hash of bid info
+ */
+export function getBidHash(price: bigint, size: bigint, blindingFactor: bigint): string {
     let msg = constructBidInfo(price, size, blindingFactor)
     let hash = sha3_256(msg.toBytes());
     return hash
 }
 
-// serialize bid info to BCS buffer - should be same as onchain
-export function constructBidInfo(price: number, size: number, blindingFactor: number): BcsWriter {
+/**
+ * Serialize bid info to BCS buffer - should be same as onchain
+ * @param {bigint} price bid price
+ * @param {bigint} size bid size
+ * @param {bigint} blindingFactor a random number
+ * @returns {BcsWriter} the serialized bid info
+ */
+export function constructBidInfo(price: bigint, size: bigint, blindingFactor: bigint): BcsWriter {
     return new BcsWriter().write64(price).write64(size).write64(blindingFactor)
 }
 
