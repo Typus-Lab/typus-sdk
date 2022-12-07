09099f4b19004f0a9f70c9363e0e62400c07d697
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-07 13:53:36 +0800

    rename

--- scripts/coveredCall/getDepositTx.ts
+++ scripts/coveredCall/getDepositTx.ts
@@ -1,4 +1,4 @@
-import { getCoveredCallDepositTx } from "../../utils/coveredCall/getCoveredCallDepositTx"
+import { getDepositTx } from "../../utils/coveredCall/getDepositTx"
 import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT } from "../../constants"
 (async () => {
     console.log("test for getDepositTx()")
@@ -8,6 +8,6 @@ import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT } fr
     let coin = ""
     let amount = 9999
 
-    let coveredCallDepositTx: any = await getCoveredCallDepositTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, index, rolling, coin, amount);
+    let coveredCallDepositTx: any = await getDepositTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, index, rolling, coin, amount);
     console.log(coveredCallDepositTx)
 })()
--- scripts/coveredCall/getUnsubscribeTx.ts
+++ scripts/coveredCall/getUnsubscribeTx.ts
@@ -1,5 +1,5 @@
-import { getCoveredCallUnsubscribeTx } from "../../utils/coveredCall/getCoveredCallUnsubscribeTx"
-import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, COVERED_CALL_MANAGER, DEFAULT_TYPE_ARGUMENT } from "../../constants"
+import { getCoveredCallUnsubscribeTx } from "../../utils/coveredCall/getUnsubscribeTx"
+import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT } from "../../constants"
 (async () => {
     console.log("test for getCoveredCallUnsubscribeTx()")
 
