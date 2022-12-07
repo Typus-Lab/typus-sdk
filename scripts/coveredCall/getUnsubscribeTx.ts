54cb8221ff34569a6d448ac51d2478f233ecba2f
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-07 13:54:05 +0800

    rename

--- scripts/coveredCall/getUnsubscribeTx.ts
+++ scripts/coveredCall/getUnsubscribeTx.ts
@@ -1,10 +1,10 @@
-import { getCoveredCallUnsubscribeTx } from "../../utils/coveredCall/getUnsubscribeTx"
+import { getUnsubscribeTx } from "../../utils/coveredCall/getUnsubscribeTx"
 import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT } from "../../constants"
 (async () => {
-    console.log("test for getCoveredCallUnsubscribeTx()")
+    console.log("test for getUnsubscribeTx()")
 
     let index = 0
 
-    let coveredCallUnsubscribeTx: any = await getCoveredCallUnsubscribeTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, index);
+    let coveredCallUnsubscribeTx: any = await getUnsubscribeTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, index);
     console.log(coveredCallUnsubscribeTx)
 })()
