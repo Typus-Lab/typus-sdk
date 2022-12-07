6cfcd74a59663835df9f6932f4314f0bc9f603ce
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-07 16:43:20 +0800

    hotfix

--- scripts/coveredCall/getNewBidTx.ts
+++ scripts/coveredCall/getNewBidTx.ts
@@ -1,12 +1,13 @@
 import { getNewBidTx } from "../../utils/coveredCall/getNewBidTx"
-import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, TIME } from "../../constants"
+import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT } from "../../constants"
 (async () => {
     console.log("test for getNewBidTx()")
 
     let index = 0
     let size = 1
     let coin = ""
+    let time = ""//publish from typus-oracle->new_time()
 
-    let newBidTx: any = await getNewBidTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, index, size, coin, TIME);
+    let newBidTx: any = await getNewBidTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, index, size, coin, time);
     console.log(newBidTx)
 })()
--- scripts/coveredCall/getNewCoveredCallVaultTx.ts
+++ scripts/coveredCall/getNewCoveredCallVaultTx.ts
@@ -1,12 +1,13 @@
 import { getNewCoveredCallVaultTx } from "../../utils/coveredCall/getNewCoveredCallVaultTx"
-import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, COVERED_CALL_MANAGER, DEFAULT_TYPE_ARGUMENT, PRICE_ORACLE } from "../../constants"
+import { COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, COVERED_CALL_MANAGER, DEFAULT_TYPE_ARGUMENT } from "../../constants"
 (async () => {
     console.log("test for getNewCoveredCallVaultTx()")
 
     let expiration = 123
     let assetName = "sui"
     let strike = 456
+    let oracle = "0x7158f68a06bc09123fcba1645f4c56ca866a8e90" //created from "new oracle"
 
-    let newCoveredCallVaultTx: any = await getNewCoveredCallVaultTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, COVERED_CALL_MANAGER, expiration, assetName, strike, PRICE_ORACLE);
+    let newCoveredCallVaultTx: any = await getNewCoveredCallVaultTx(COVERED_CALL_PACKAGE, COVERED_CALL_REGISTRY, DEFAULT_TYPE_ARGUMENT, COVERED_CALL_MANAGER, expiration, assetName, strike, oracle);
     console.log(newCoveredCallVaultTx)
 })()
