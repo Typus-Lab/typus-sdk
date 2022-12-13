e260206f6b3ec7b82119470929db4ea9e47cce2e
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-13 15:19:44 +0800

    update constant

--- constants.ts
+++ constants.ts
@@ -1,14 +1,14 @@
 //publish from sui-dev-token
 export const TOKEN_PACKAGE = "0x0a7a15d173690ac676b03eb8285a615a8982cb27";
-export const TOKEN_REGISTRY = "0x25c417ee04e789911fdd4b6edabbeb4320fe7eea";//fake btc registry
+export const TOKEN_REGISTRY = "0x7fe9f4e9c4cb004a19cd5b5b2cc71509f94780aa";//fake btc registry
 
 //publish from typus-oracle
 export const ORACLE_PACKAGE = "0xe85eabdada216e470344b2e70cdc8155a4007a2a"
 
 //publish from typus-dov/cover_call
-export const COVERED_CALL_PACKAGE = "0xc33f18cc20a5c17c41ef9e054e1b37f60d804d9c"
-export const COVERED_CALL_REGISTRY = "0x6c17473d6b1252db26278ec5818ef3bfdca75c89"
-export const COVERED_CALL_MANAGER = "0xe004885ef67c7af37b96f8313738bb758d51b6d3"
+export const COVERED_CALL_PACKAGE = "0x76ed224498d2be57bf77ec8c0015957dd6096a36"
+export const COVERED_CALL_REGISTRY = "0x4a3b7c389f5f7a506317774a49e39631ff160537"
+export const COVERED_CALL_MANAGER = "0x35f69cc3dff02a9e86616569a72a0c8e9d950cca"
 
 //publish from typus-dov/typus-dov
 export const DOV_PACKAGE = "0x461c7f49f322c3b12523f51c8b8f00d6e0eda243"
