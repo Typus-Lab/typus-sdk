155318d80a9119786e7b243ed3a011a465388f2a
jericlong <jeric.l@alphabit.software>
2022-12-14 11:46:40 +0800

    hotfix utils/getSubVault.ts

--- utils/getSubVault.ts
+++ utils/getSubVault.ts
@@ -3,6 +3,10 @@ import { COVERED_CALL_REGISTRY } from "../constants"
 const provider = new JsonRpcProvider(Network.DEVNET);//for read only operations
 
 export async function subVaults(): Promise<any> {
+    // let data = await provider.getObject("0x9f3a10e5e7cc22092825c9a4e8269b37fb935a9d")
+    // let data = await provider.getObjectsOwnedByObject("0x0fda36c64af91d4f9c946f00c6f13194c37493ab")
+    // let data = await provider.getObject("0x933ab335ef83ef247ae43f537f9a76317feb7358")
+    // console.log("data: ", JSON.stringify(data))
     return await subVaultsV2(COVERED_CALL_REGISTRY, provider)
 }
 
@@ -14,18 +18,21 @@ export async function subVaultsV2(registry: string, provider: JsonRpcProvider): 
     console.log("vault num under registry:" + tmpObj1.length)
     if (tmpObj1.length != 0) {
         let vault = tmpObj1[0].objectId
+        // let vault = "0x701c014a20294ee7e170148353a2dfe42fb5fb88"
         console.log("vault: " + vault)
 
         //table
         let tmpObj2 = await provider.getObject(vault)
+
         //@ts-ignore
-        let table = tmpObj2.details.data.fields.value.fields.sub_vaults.fields.id.id
-        console.log("table: " + table)
+        let table_id = tmpObj2.details.data.fields.value.fields.vault.fields.sub_vaults.fields.id.id
+        console.log("table: " + table_id)
 
         //sub vault (maker / rolling / no_rolling) id 
-        let tmpObj3 = await provider.getObjectsOwnedByObject(table)
+        let tmpObj3 = await provider.getObjectsOwnedByObject(table_id)
         let subVaultsId: string[] = [];
         tmpObj3.map(e => {
+            // console.log("data: " + JSON.stringify(e))
             subVaultsId.push(e.objectId as string)
         }
         )
@@ -48,3 +55,5 @@ export async function subVaultsV2(registry: string, provider: JsonRpcProvider): 
         return null;
     }
 }
+
+subVaults()
