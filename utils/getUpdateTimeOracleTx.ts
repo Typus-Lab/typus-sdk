164bef2e3ae0302afd0d1c3eb127f8080dec3b0e
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-14 17:40:57 +0800

    update time oracle payload

---
+++ utils/getUpdateTimeOracleTx.ts
@@ -0,0 +1,16 @@
+export async function getUpdateTimeOracleTx(packageId: string, oracle: string, managerCap: string, ts: number): Promise<any> {
+    let tx = {
+        packageObjectId: packageId,
+        module: 'unix_time',
+        function: 'update',
+        typeArguments: [],
+        arguments: [
+            oracle,
+            managerCap,
+
+            ts
+        ],
+        gasBudget: 1000,
+    }
+    return tx
+}
