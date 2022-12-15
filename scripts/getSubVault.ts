602b165207de3925c20d80134a3b06e2860b491e
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-15 14:50:30 +0800

    hotfix

--- scripts/getSubVault.ts
+++ scripts/getSubVault.ts
@@ -1,5 +1,5 @@
-import { subVaults } from '../utils/getSubVault';
-(async () => {
-    let res = await subVaults()//TODO: let registry be input variable
-    console.log(res)
-})();
+// import { subVaults } from '../utils/getSubVault';
+// (async () => {
+//     let res = await subVaults()//TODO: let registry be input variable
+//     console.log(res)
+// })();
