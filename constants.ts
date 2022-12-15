339710f13322946f96f61b6e5ef039270922bb19
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-15 10:49:40 +0800

    update constant

--- constants.ts
+++ constants.ts
@@ -1,8 +1,9 @@
 //publish from sui-dev-token
-export const TOKEN_PACKAGE = "0x0a7a15d173690ac676b03eb8285a615a8982cb27";
-export const TOKEN_REGISTRY_BTC = "0x7fe9f4e9c4cb004a19cd5b5b2cc71509f94780aa";//fake btc registry
-export const TOKEN_REGISTRY_ETH = "0xa1e5ae3365f54f2f0addba26d1843a99a3d04eab"//fake eth registry
-export const TOKEN_REGISTRY_USDC = "0xcd64daa636a19c8ba55422167a22b20ff7fbbfde"//fake usdc registry
+export const TOKEN_PACKAGE = "0x04aed544785e606bbda1100b7af9aaefb1db60b3";
+export const TOKEN_REGISTRY_BTC = "0x1d41542c9bcbb3427e80afb6cd7f05b304feb149";//fake btc registry
+export const TOKEN_REGISTRY_ETH = "0x97da9ec6f9842025480b73a2249e8670491c37d6"//fake eth registry
+export const TOKEN_REGISTRY_SUI = "0xa0b7b5fc89ee0fa8a1f3355f9370c9651f7611b6"//fake usdc registry
+
 
 //publish from typus-oracle
 export const ORACLE_PACKAGE = "0xe85eabdada216e470344b2e70cdc8155a4007a2a"
@@ -18,11 +19,11 @@ export const DOV_PACKAGE = "0x461c7f49f322c3b12523f51c8b8f00d6e0eda243"
 export const DEFAULT_TYPE_ARGUMENT = "0x2::sui::SUI";
 export const FAKE_TOKEN_TYPE_ARGUMENT = "0x27b3674c685046f66cad1d5496d2967894fa5329::token::USDC";
 export const TEST_MNEMONIC = "plug grain matter asthma aware critic can lottery blouse uncover sauce speak";
-export const TOKEN_NAME = ["BTC", "ETH", "SUI", "USDC", "USDT"]
+export const TOKEN_NAME = ["BTC", "ETH", "SUI", "USDC", "USDT", "SUI"]
 export const TOKEN_NAME_TO_MODULE = {
     "BTC": "btc",
     "ETH": "eth",
-    // "SUI": "token_sui",
+    "SUI": "sui",
     "USDC": "usdc",
     // "USDT": "token_usdt",
 }
