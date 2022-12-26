291d7873797609866872a6c0a14f604286cd882a
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-26 11:12:45 +0800

    update constant

--- constants.ts
+++ constants.ts
@@ -1,20 +1,20 @@
 //publish from sui-dev-token
-export const TOKEN_PACKAGE = "0xe1de18bfb66277f0abed845f868af6c962248f72";
-export const TOKEN_REGISTRY_BTC = "0x19702808d1c5d5534636cf7a31f90a60da6194cd";//fake btc registry
-export const TOKEN_REGISTRY_ETH = "0x06148f59eec7eee47acedb98f61819572f2bafde"//fake eth registry
-export const TOKEN_REGISTRY_SUI = "0x1c4ccc2fab72030abf8b04af20408a3c1c8dae5f"//fake usdc registry
+export const TOKEN_PACKAGE = "0x2009591841eff19d6c5e97eae7a45f2f3908e502";
+export const TOKEN_REGISTRY_BTC = "0x86d252653e78b9f7c7b5382f93f5c74852544e1c";//fake btc registry
+export const TOKEN_REGISTRY_ETH = "0x569832dd430ea3d31853b2e93e545bde89526021"//fake eth registry
+export const TOKEN_REGISTRY_SUI = "0xde87ddc6cc6570ae8ff3bf9ab4cf07b2d5f7bba3"//fake usdc registry
 
 
 //publish from typus-oracle
-export const ORACLE_PACKAGE = "0x76d673a3f7aa558016e692b4de02c71de11f00b7"
+export const ORACLE_PACKAGE = "0x136ff71dc0e8cb679f49b095e6dff5943e35cdf1"
 
 //publish from typus-dov/cover_call
-export const COVERED_CALL_PACKAGE = "0x04ec35903020610909d274390e94612ce679f635"
-export const COVERED_CALL_REGISTRY = "0x052f3b32669d6b92f485f97310add8adf1dc7478"
-export const COVERED_CALL_MANAGER = "0x5b5bb3fefabd314cb57fc6f667d46ba4d76373e1"
+export const COVERED_CALL_PACKAGE = "0x7a08fbb9c4682a98ad575254e9f6886d2f7baf81"
+export const COVERED_CALL_REGISTRY = "0xe22a98459592becac638c08fff7ee252fbee9e37"
+export const COVERED_CALL_MANAGER = "0x9bedfe695cd7b38673d0bd31dafb1d937ef39904"
 
 //publish from typus-dov/typus-dov
-export const DOV_PACKAGE = "0xfaadf1d975ed3dec39cbe93a1f44c034966f6747"
+export const DOV_PACKAGE = "0x6d82e0416683c7b5f9cac0a0ce2b25f01a34e628"
 
 export const DEFAULT_TYPE_ARGUMENT = "0x2::sui::SUI";
 export const FAKE_TOKEN_TYPE_ARGUMENT = "0x27b3674c685046f66cad1d5496d2967894fa5329::token::USDC";
