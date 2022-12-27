9db13b665e8c3efc0f88c7eefa2736e27e50f0db
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2022-12-27 15:29:43 +0800

    update constant

--- constants.ts
+++ constants.ts
@@ -8,12 +8,14 @@ export const TOKEN_REGISTRY_SUI = "0x9ded8015e2ce496cee989b6701e69c73e86436e6"//
 export const ORACLE_PACKAGE = "0x7d683916b32c25b0061e65d3c744838cea225fe9"
 
 //publish from typus-dov/cover_call
-export const COVERED_CALL_PACKAGE = "0x7a08fbb9c4682a98ad575254e9f6886d2f7baf81"
-export const COVERED_CALL_REGISTRY = "0xe22a98459592becac638c08fff7ee252fbee9e37"
-export const COVERED_CALL_MANAGER = "0x9bedfe695cd7b38673d0bd31dafb1d937ef39904"
+export const COVERED_CALL_PACKAGE = "0x56dbf64116e1e70353f4b35e0c708a194d04dbb7"
+export const COVERED_CALL_REGISTRY = "0x0183b44c412e7713bb928c330bd0cbfb8817ff75"
+export const COVERED_CALL_MANAGER = "0x2370429bc93b9861b45d542862bbfd0711f4d686"
+export const USER_SHARE_TABLE = "0xd0765bf26da16c2debf6e136c0fcab67f2054ac3"
+export const MAKER_SHARE_TABLE = "0x762ad4e903b45f0a5e451874d30bce777a69f0f6"
 
 //publish from typus-dov/typus-dov
-export const DOV_PACKAGE = "0x6d82e0416683c7b5f9cac0a0ce2b25f01a34e628"
+export const DOV_PACKAGE = "0xa6a4607b52f5a2a6299434c8c3e5040e34c325d7"
 
 export const DEFAULT_TYPE_ARGUMENT = "0x2::sui::SUI";
 export const FAKE_TOKEN_TYPE_ARGUMENT = "0x27b3674c685046f66cad1d5496d2967894fa5329::token::USDC";
