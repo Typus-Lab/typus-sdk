5d0109fad68c1ff9793c9d674833548cacc3925c
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2023-01-04 14:16:08 +0800

    update get vault data

--- test/getVaultData.ts
+++ test/getVaultData.ts
@@ -1,8 +1,8 @@
 import { COVERED_CALL_REGISTRY } from "../constants"
 import { getVaultDataFromRegistry } from "../utils/getVaultData"
-
+import { CoveredCallVault } from "../utils/fetchData"
 (async () => {
-    let res = await getVaultDataFromRegistry(COVERED_CALL_REGISTRY);
+    let res: CoveredCallVault[] = await getVaultDataFromRegistry(COVERED_CALL_REGISTRY);
     console.log("vault: ")
     console.log(res)
 })()
--- utils/getVaultData.ts
+++ utils/getVaultData.ts
@@ -1,31 +1,31 @@
 
 import { JsonRpcProvider, Network } from '@mysten/sui.js';
 import { TOKEN_NAME } from '../constants';
-import { Vault, PayoffConfig } from "../utils/fetchData"
+import { CoveredCallVault, PayoffConfig, Config, VaultConfig, Vault } from "../utils/fetchData"
 
 const provider = new JsonRpcProvider(Network.DEVNET);//for read only operations
 
-export async function getVaultDataFromRegistry(registry: string): Promise<Vault[]> {
+export async function getVaultDataFromRegistry(registry: string): Promise<CoveredCallVault[]> {
     console.log("registry: " + registry)
 
     let coveredCallVaults: any[] = await provider.getObjectsOwnedByObject(registry)
     let coveredCallVaultsId: string[] = coveredCallVaults.map(e => e.objectId as string)
     let objsInfo = await provider.getObjectBatch(coveredCallVaultsId)
 
-    let vaults: Vault[] = [];
+    let vaults: CoveredCallVault[] = [];
 
     for (let objInfo of objsInfo) {
         if (objInfo.status != "Exists") {
             console.log("obj not exist")
             continue
         }
-        //vault
-        //@ts-ignore
-        let vault = objInfo.details.data.fields.value.fields.vault.fields
 
-        //config
         //@ts-ignore
-        let config = objInfo.details.data.fields.value.fields.config.fields
+        // console.log(objInfo.details.data.fields.value.fields.config.fields)
+
+        //vaultId
+        //@ts-ignore
+        let vaultId = objInfo.details.data.fields.id.id
 
         //vaultIdx
         //@ts-ignore
@@ -40,30 +40,72 @@ export async function getVaultDataFromRegistry(registry: string): Promise<Vault[
             asset = ""
         }
 
-        //vaultId
+        //config
         //@ts-ignore
-        let vaultId = objInfo.details.data.fields.id.id
+        let config = objInfo.details.data.fields.value.fields.config.fields
 
-        //payoff config
-        let payoffConfig: PayoffConfig = {
-            exposureRatio: Number(config.payoff_config.fields.exposure_ratio),
-            premiumRoi: Number(config.payoff_config.fields.premium_roi),
-            strike: Number(config.payoff_config.fields.strike),
-            strikeOtmPct: Number(config.payoff_config.fields.strike_otm_pct)
+        //vault
+        //@ts-ignore
+        let vault = objInfo.details.data.fields.value.fields.vault.fields
+
+        //prevBalance
+        //@ts-ignore
+        let prevBalance = objInfo.details.data.fields.value.fields.prev_balance as number
+
+        let vaultConfig = config.vault_config.fields
+        let vaultConfigRes: VaultConfig = {
+            strikeOtmPct: vaultConfig.strike_otm_pct,
+            strikeIncrement: vaultConfig.strike_increment,
+            decaySpeed: vaultConfig.decay_speed,
+            initialPrice: vaultConfig.initial_price,
+            finalPrice: vaultConfig.final_price,
+            auctionDurationInMs: vaultConfig.auction_duration_in_ms,
         }
 
-        let res: Vault = {
+        let nextVaultConfig = config.next_vault_config.fields
+        let nextVaultConfigRes: VaultConfig = {
+            strikeOtmPct: nextVaultConfig.strike_otm_pct,
+            strikeIncrement: nextVaultConfig.strike_increment,
+            decaySpeed: nextVaultConfig.decay_speed,
+            initialPrice: nextVaultConfig.initial_price,
+            finalPrice: nextVaultConfig.final_price,
+            auctionDurationInMs: nextVaultConfig.auction_duration_in_ms,
+        }
+
+        let payoffConfig = config.payoff_config.fields
+        let payoffConfigRes: PayoffConfig = {
+            exposureRatio: Number(payoffConfig.exposure_ratio),
+            premiumRoi: Number(payoffConfig.premium_roi),
+            strike: Number(payoffConfig.strike),
+        }
+
+        let configRes: Config = {
+            period: config.period,// daily:0 weekly:1 monthly:2
+            activationTsMs: config.activation_ts_ms,
+            expirationTsMs: config.expiration_ts_ms,
+            tokenDecimal: config.token_decimal,
+            shareDecimal: config.share_decimal,
+            capacity: config.capacity,
+            vaultConfig: vaultConfigRes,
+            nextVaultConfig: nextVaultConfigRes,
+            payoffConfig: payoffConfigRes,
+        }
+
+        let vaultRes: Vault = {
+            ableToDeposit: vault.able_to_deposit,
+            ableToWithdraw: vault.able_to_withdraw,
+        }
+
+        let tvl = Number(vault.regular_sub_vault.fields.balance) + Number(vault.rolling_sub_vault.fields.balance)
+
+        let res: CoveredCallVault = {
             vaultId: vaultId,
             vaultIdx: vaultIdx,
             asset: asset,
-            ableToDeposit: vault.able_to_deposit,
-            ableToWithdraw: vault.able_to_withdraw,
-            expirationTsMs: config.expiration_ts_ms,
-            payoffConfig: payoffConfig,
-            period: Number(config.period),// daily:0 weekly:1 monthly:2
-            shareDecimal: config.share_decimal,
-            startTsMs: config.start_ts_ms,
-            tokenDecimal: config.token_decimal,
+            config: configRes,
+            vault: vaultRes,
+            prevBalance: prevBalance,
+            tvl: tvl,
         }
 
         vaults.push(res)
