ec154541767aa4b763d586b4d830c0fbc8c81144
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2023-01-05 16:22:21 +0800

    hotfix

--- utils/coveredCall/getNewAuctionTx.ts
+++ utils/coveredCall/getNewAuctionTx.ts
@@ -1,4 +1,4 @@
-export async function getNewAuctionTx(packageId: string, registry: string, typeArgument: string, managerCap: string, index: number): Promise<any> {
+export async function getNewAuctionTx(packageId: string, registry: string, typeArgument: string, managerCap: string, index: number, priceOracle: string, timeOracle: string): Promise<any> {
     let tx = {
         packageObjectId: packageId,
         module: 'covered_call',
@@ -8,6 +8,8 @@ export async function getNewAuctionTx(packageId: string, registry: string, typeA
             managerCap,
             registry,
             index,
+            priceOracle,
+            timeOracle,
         ],
         gasBudget: 1000,
     }
