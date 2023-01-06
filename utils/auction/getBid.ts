0cead39cb5804430199646a9b72c34244d9288a5
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2023-01-06 11:41:58 +0800

    update bid data

--- utils/auction/getBid.ts
+++ utils/auction/getBid.ts
@@ -1,5 +1,6 @@
 
 import { JsonRpcProvider, Network } from '@mysten/sui.js';
+import { TOKEN_DECIMAL } from '../../constants';
 import { Bid } from "../fetchData"
 const provider = new JsonRpcProvider(Network.DEVNET);//for read only operations
 
@@ -22,7 +23,7 @@ export async function getBid(vault: string): Promise<Bid[]> {
         let bidData = e.details.data.fields.value.fields
         let res: Bid = {
             price: Number(bidData.price),
-            size: Number(bidData.size),
+            size: Number(bidData.size / (10 ** TOKEN_DECIMAL)),
             tsMs: Number(bidData.ts_ms),
         }
         return res
