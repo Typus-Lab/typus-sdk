fa29c4b662a3e598300316896d670f8fbaffd214
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2023-01-06 14:36:58 +0800

    get update next vault config

---
+++ utils/coveredCall/getUpdateNextVaultConfigTx.ts
@@ -0,0 +1,31 @@
+export async function getUpdateNextVaultConfigTx(
+    packageId: string,
+    registry: string,
+    typeArgument: string,
+    index: string,
+    strikeOtmPct: string,
+    strikeIncrement: string,
+    decaySpeed: string,
+    initialPrice: string,
+    finalPrice: string,
+    auctionDurationInMs: string,
+): Promise<any> {
+    let tx = {
+        packageObjectId: packageId,
+        module: 'covered_call',
+        function: 'update_next_vault_config',
+        typeArguments: [typeArgument],
+        arguments: [
+            registry,
+            index,
+            strikeOtmPct,
+            strikeIncrement,
+            decaySpeed,
+            initialPrice,
+            finalPrice,
+            auctionDurationInMs,
+        ],
+        gasBudget: 1000,
+    }
+    return tx
+}
