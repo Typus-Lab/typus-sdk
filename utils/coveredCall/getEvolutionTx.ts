04619786e104777e32a79da28a2e807d0c1cc8fe
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2023-01-06 15:01:18 +0800

    evolution tx

---
+++ utils/coveredCall/getEvolutionTx.ts
@@ -0,0 +1,29 @@
+export async function getEvolutionTx(
+    packageId: string,
+    registry: string,
+    typeArgument: string,
+    managerCap: string,
+    index: string,
+    priceOracle: string,
+    timeOracle: string,
+    activationTsMs: string,
+    expirationTsMs: string,
+): Promise<any> {
+    let tx = {
+        packageObjectId: packageId,
+        module: 'covered_call',
+        function: 'evolution',
+        typeArguments: [typeArgument],
+        arguments: [
+            managerCap,
+            registry,
+            index,
+            priceOracle,
+            timeOracle,
+            activationTsMs,
+            expirationTsMs,
+        ],
+        gasBudget: 10000,
+    }
+    return tx
+}
