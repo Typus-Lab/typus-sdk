d991473afaffcdbb5acedf8cb1faf692b8b44afe
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2023-01-12 15:34:29 +0800

    event listener

--- test/eventListener.ts
+++ test/eventListener.ts
@@ -1,17 +1,28 @@
 
-import { TEST_MNEMONIC, DOV_PACKAGE, TOKEN_PACKAGE } from "../constants"
+import { TEST_MNEMONIC, DOV_PACKAGE, TOKEN_PACKAGE, COVERED_CALL_PACKAGE } from "../constants"
 import { JsonRpcProvider, Ed25519Keypair, RawSigner, Network, SuiEventEnvelope, SuiEvent } from '@mysten/sui.js';
 import cron from "node-cron";
 import { string } from "superstruct";
+import { XMLHttpRequest } from "XMLHttpRequest"
+import moment from "moment"
 const provider = new JsonRpcProvider(Network.DEVNET);//for read only operations
+let apiToken = "5864284783:AAHwXWgt2YgLENdJ9mVBUDBVLHXrMLNgkic";
+
+/*
+「https://api.telegram.org/botTOKEN/getUpdates」，change TOKEN to token from botFather
+*/
+let chatId = "-1001784476809";
 
 (async () => {
     let bidType = DOV_PACKAGE + "::dutch::NewBid<" + TOKEN_PACKAGE + "::eth::ETH>"
 
+    let newAuctionType = COVERED_CALL_PACKAGE + "::covered_call::NewAuction"
+
     let renewSec = 10
 
-    await getBidEventsCranker(bidType, renewSec)
+    // await getBidEventsCranker(bidType, renewSec)
 
+    await getNewAuctionEventsCranker(newAuctionType, renewSec)
 })()
 
 export async function getBidEventsCranker(type: string, renewSec: number) {
@@ -36,7 +47,6 @@ export async function getBidEventsCranker(type: string, renewSec: number) {
             console.log("the total bid event number now:" + events.data.length)
             console.log("there are " + (newRes.length - res.length).toString() + " new bids")
 
-
             //newBid: the newest event in this round
             let newBid = newRes.filter((e) => {
                 return res.indexOf(e) === -1
@@ -52,4 +62,52 @@ export async function getBidEventsCranker(type: string, renewSec: number) {
         // })
 
     });
-} 
+}
+
+export async function getNewAuctionEventsCranker(type: string, renewSec: number) {
+    let res: any[] = [];
+
+    cron.schedule('*/' + renewSec.toString() + ' * * * * *', async () => {
+        console.log("listening for every " + renewSec.toString() + " s...")
+        const events = await provider.getEvents(
+            { MoveEvent: type },
+            null,
+            null,
+        )
+
+        let newRes: any[] = events.data
+
+        if (newRes.length != res.length) {
+            console.log("start a new auction!")
+            console.log(events)
+
+            let newAuction = newRes.filter((e) => {
+                return res.indexOf(e) === -1
+            })
+            let timesFormat: string = ""
+            newAuction.map(e => {
+                let time = moment.unix(e.timestamp / 1000).format("YYYY MM/DD HH:mm:ss")
+                console.log(time)
+                timesFormat += time + " \n"
+            })
+
+            let telegramText: string = "start a new auction! Time: \n" + timesFormat
+            sendEventToTelegramChannel(telegramText)
+
+            res = newRes
+        }
+    });
+}
+
+export async function sendEventToTelegramChannel(text: any) {
+
+    let urlString = `https://api.telegram.org/bot${apiToken}/sendMessage?chat_id=${chatId}&text=${text}`;
+
+    let request = new XMLHttpRequest();
+    request.open("GET", urlString);
+    request.send();
+
+    // request.onload = function () {
+    //     console.log(JSON.parse(request.responseText));
+    // };
+}
