9b46920e1f5adc495c07591cfceee5a112fda701
LeoOptiFi <102012806+LeoOptiFi@users.noreply.github.com>
2023-01-16 13:24:42 +0800

    update constant

--- constants.ts
+++ constants.ts
@@ -1,21 +1,23 @@
 //publish from sui-dev-token
-export const TOKEN_PACKAGE = "0x1ed338b4e737d89729386b8170b0f45910ad30fa";
-export const TOKEN_REGISTRY_BTC = "0xf5f939a6a79927e55582985a8bf125d37b0ab297";//fake btc registry
-export const TOKEN_REGISTRY_ETH = "0xb79efcacd84dc70dd6969686d7e7fadadc14c2d5"//fake eth registry
-export const TOKEN_REGISTRY_SUI = "0xde095084d4485b0482ab8dc8fe8c8328902fa223"//fake usdc registry
-
+export const TOKEN_PACKAGE = "0x3e416d10557c532838c2d0d6996c8a3bc81371a7"
+export const TOKEN_REGISTRY_BTC = "0xee26a4debb44a6c09e0f446c5c98da2217a0ca5d"
+export const TOKEN_REGISTRY_ETH = "0x00c7526bc0b4b7be9fb6d23e60e325700194e018"
+export const TOKEN_REGISTRY_SUI = "0x342f858a4f813a8f95ddc216ddf7bcb775a92782"
+export const TOKEN_REGISTRY_APT = "0x09de00969635499a7f824688bff570efbf886a68"
+export const TOKEN_REGISTRY_DOGE = "0xa33ba6e2c736a2548b8c1cef13fed947c6aae163"
+export const TOKEN_REGISTRY_USDC = "0xf22cd5b80e1263e436fb4d71879ebb1b2799aa36"
 //publish from typus-oracle
-export const ORACLE_PACKAGE = "0x5b1f5680041041624069ef03f23947001b073642"
+export const ORACLE_PACKAGE = "0xcba43b3a1672f3c782e2167fc64710799009d5bb"
 
-export const TIME_ORACLE = "0x1480773700cdeecfbac028f5345ec8f472750ef8"
+export const TIME_ORACLE = "0x55031e1edc517a4b16543c1c081ba33a59ffd084"
 
 //publish from typus-dov/cover_call
-export const COVERED_CALL_PACKAGE = "0x00d18316601f16439dec0a6b3e40ef861fc84ee4"
-export const COVERED_CALL_REGISTRY = "0xe3960d9dc8e39328e514d42d3318c5878145c27e"
-export const COVERED_CALL_MANAGER = "0xfe26a77c66f4ae0314e1510921f8dea9be01db9c"
-export const USER_SHARE_TABLE = "0x4d98c1efa4aef5b02e8fe27bca6ac4d8dde62935"
-export const USER_REFUND_TABLE = "0x3e80b4c0f7d1b05a73d180487aa2bbf5e3c77792"
-export const MAKER_SHARE_TABLE = "0xef0dabd3f82d4cc09c440a61f095a977d72ade05"
+export const COVERED_CALL_PACKAGE = "0xb92f3c4c26151b86977b8811765fe74cb133a35f"
+export const COVERED_CALL_REGISTRY = "0x0a4bd1721cb02a957d1602f5b2e53ad421408acb"
+export const COVERED_CALL_MANAGER = "0x6fd90bf4bb9490e5fce51b8eb11fbf37c43f4cbc"
+export const USER_SHARE_TABLE = "0xc90f9f13f9823719011a9b2de73bb4d6e05eaf28"
+export const USER_REFUND_TABLE = "0x2a63900582a5177f7ff440613ba091618f373bfa"
+export const MAKER_SHARE_TABLE = "0xfcfd04446aa3dd004150081df8982c1f09b20fb1"
 
 //publish from typus-dov/typus-dov
 export const DOV_PACKAGE = "0x7d808c68990f1c1f54efeda8bbbe3a9746ce4026"
@@ -26,8 +28,8 @@ export const PRICE_DECIMAL = 8
 export const TOKEN_DECIMAL = 9
 
 export const DEFAULT_TYPE_ARGUMENT = "0x2::sui::SUI";
-export const FAKE_TOKEN_TYPE_ARGUMENT = "0x27b3674c685046f66cad1d5496d2967894fa5329::token::USDC";
-export const TEST_MNEMONIC = "plug grain matter asthma aware critic can lottery blouse uncover sauce speak";
+export const FAKE_TOKEN_TYPE_ARGUMENT = "0x27b3674c685046f66cad1d5496d2967894fa5329::token::USDC"
+export const TEST_MNEMONIC = "plug grain matter asthma aware critic can lottery blouse uncover sauce speak"
 export const TOKEN_NAME = ["BTC", "ETH", "SUI", "USDC", "USDT", "SUI"]
 export const TOKEN_NAME_TO_MODULE = {
     "BTC": "btc",
