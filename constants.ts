fa8703eb7e1f1060e7d8fb57b37cbb4eb0b2ee44
LeoOptifi <chun.w@alphabit.software>
2023-02-09 14:13:58 +0800

    update constant

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 //publish from sui-dev-token
-export const TOKEN_PACKAGE = "0x232b50236cb72e062e5a028614ba5e0df666e0d5"
+export const TOKEN_PACKAGE = "0xcb6f5de70c65b988556b93988a58a57919584e56"
 export const TOKEN_REGISTRY_BTC = "0x7325b75b85ee45e78204a9dcf7800a397259eb3d"
 export const TOKEN_REGISTRY_ETH = "0x475d3b10d29c03d85dfa707b5479955729216d4d"
 export const TOKEN_REGISTRY_SUI = "0x3b87b3f0403f527d28fd1301074a12446f43817a"
@@ -7,17 +7,17 @@ export const TOKEN_REGISTRY_APT = "0xf1a947ebc2643942bf3f2ecbd6b583c0f8dc3baf"
 export const TOKEN_REGISTRY_DOGE = "0x4dbe73f8a744008bc20d12aaa0d4fc8f01692358"
 export const TOKEN_REGISTRY_USDC = "0x82e0a1473c6c4d4f713a0ac5957e0c7a2278ed1e"
 //publish from typus-oracle
-export const ORACLE_PACKAGE = "0x63b5ef3c1858e196a1340988d96ad35fe1fcb06f"
+export const ORACLE_PACKAGE = "0xddfa73b1bf15da6f72da9346a16051b7b90db1a6"
 
-export const TIME_ORACLE = '0x5f717110e094686d6673c1e1ecf0e47f22e88842';
+export const TIME_ORACLE = '0xb4a661b1493812476558de89af2092245b2d893f';
 
 //publish from typus-dov/cover_call
-export const COVERED_CALL_PACKAGE = "0xb5ddd49ba8329599d68511979b6a0512f5164db9"
-export const COVERED_CALL_REGISTRY = "0x4fd15766082b199930d25e6cd1288029da74a7c6"
-export const COVERED_CALL_MANAGER = "0xe00532cbf1e6c724e885b01c059b8c0da0a5f5bf"
-export const USER_SHARE_TABLE = "0xb2cb82f755dad2346777051f8a637b3eaa69445f"
-export const USER_REFUND_TABLE = "0xa29a7f3c278a1c4263b6ae8bcf021448ac7abab7"
-export const MAKER_SHARE_TABLE = "0x0895223fff9aeaa627d8e4b9457c546270b01fb0"
+export const COVERED_CALL_PACKAGE = "0xdeb0b9cdc1f0da4196382a40bae55f6087c55626"
+export const COVERED_CALL_REGISTRY = "0x01212ccf4f2011efb2c0be14ed538960def8e104"
+export const COVERED_CALL_MANAGER = "0xb058fa60c70f6844d952b5f9115101bfb3cd59e6"
+export const USER_SHARE_TABLE = "0xd2c1ddb7e80dc6fe494ca4d2fcc32700eb37e098"
+export const USER_REFUND_TABLE = "0xc5683c6c8485b5ded986298dd4d15069a020b143"
+export const MAKER_SHARE_TABLE = "0x27387549841398c0792fd8472657df030294c3f0"
 
 //publish from typus-dov/typus-dov
 export const DOV_PACKAGE = "0x49e71a735dc6bdf7327e6c663d04a3e7f68bafd0"
