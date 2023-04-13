9a928f934da2b7470dc4db39cc97a408a0c7e8a8
Wayne Kuo <wayne.k@typuslab.com>
2023-04-13 13:17:41 +0800

    Create mergeCoins.ts

---
+++ test/mergeCoins.ts
@@ -0,0 +1,52 @@
+import { TEST_MNEMONIC } from "../constants";
+import {
+  JsonRpcProvider,
+  Ed25519Keypair,
+  RawSigner,
+  devnetConnection,
+  TransactionBlock,
+  testnetConnection,
+  Connection,
+} from "@mysten/sui.js";
+const connection = new Connection({ fullnode: "https://rpc-testnet.suiscan.xyz:443" });
+const provider = new JsonRpcProvider(connection); //for read only operations
+const keypair = Ed25519Keypair.deriveKeypair(TEST_MNEMONIC);
+const signer = new RawSigner(keypair, provider);
+
+(async () => {
+  const coin_type = "0x2::sui::SUI";
+
+  var coins = (
+    await provider.getAllCoins({
+      owner: await signer.getAddress(),
+    })
+  ).data;
+
+  // console.log(coins);
+
+  coins = coins.filter((c) => c.coinType == coin_type && c.lockedUntilEpoch == null);
+
+  let payment = coins.pop();
+
+  // console.log("payment", payment);
+
+  let coin_ids = coins.map((c) => c.coinObjectId);
+
+  const tx = new TransactionBlock();
+
+  tx.mergeCoins(
+    tx.pure(coin_ids.pop()),
+    coin_ids.map((id) => tx.object(id))
+  );
+
+  tx.setGasBudget(100000000);
+
+  let gas = { objectId: payment!.coinObjectId, version: payment!.version, digest: payment!.digest };
+  tx.setGasPayment([gas]);
+
+  console.log(payment);
+
+  let res = await signer.signAndExecuteTransactionBlock({ transactionBlock: tx });
+
+  console.log(res);
+})();
