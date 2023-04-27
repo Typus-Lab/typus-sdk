5a0b0115ddbeec6ad177e6c5ed8672e36be458f4
Wayne Kuo <wayne.k@typuslab.com>
2023-04-27 13:17:11 +0800

    Update sponsorTransaction.ts

--- test/sponsorTransaction.ts
+++ test/sponsorTransaction.ts
@@ -5,10 +5,10 @@ import { getDepositTx } from "../utils/portfolio/single-collateral/user/getDepos
 
 // The initiator of the transaction
 const SENDER_ADDRESS = "0xb6c7e3b1c61ee81516a8317f221daa035f1503e0ac3ae7a50b61834bc7a3ead9";
-const SENDER_RECOVERY_PHRASE = "TEST_MNEMONIC";
+const SENDER_RECOVERY_PHRASE = "";
 
 // gas budget, in MIST
-const GAS_BUDGET = 5000000;
+const GAS_BUDGET = 50000000;
 
 // Gas Station endpoint:
 const SPONSOR_RPC_URL = "https://api.shinami.com/gas/v1/sui_testnet_17aa4c66d7a325601abde1f043793003";
@@ -47,26 +47,15 @@ const sponsor = rpcClient<SponsorRpc>(SPONSOR_RPC_URL);
 
 // Different examples of programmable transaction blocks that can be crafted.
 const progTxnTransfer = async () => {
-    let gasBudget = 100000000;
     let typeArguments = [
         "0xd175cff04f1d49574efb6f138bc3b9b7313915a57b5ca04141fb1cb4f66984b2::eth::ETH",
         "0xd175cff04f1d49574efb6f138bc3b9b7313915a57b5ca04141fb1cb4f66984b2::eth::ETH",
         "0xd175cff04f1d49574efb6f138bc3b9b7313915a57b5ca04141fb1cb4f66984b2::eth::ETH",
     ];
-    let vaultIndex = "4";
-    let coins = [
-        "0x07bb1b0bd176a5e9e0377758282bd0eb27070f2684a88a2cf57964fb4da11c0d",
-        "0x2a908b66b02c450fefbede9ef02672dc2291163284e59fd90841a6024bf3aa82",
-        "0x99711f3c9a87b3ba9c9a61a6dbb4022f24799a8814c419f106cedc2c2f7b70b1",
-        "0xad5ae185437c4ef43d571e7efd188a849e6b5bf49cbd810e90b775458a8495e6",
-        "0xb9c0ef30cf9729d6ad54d3a17b524010d494eb5c16fa9b4d4859998dde485282",
-        "0xcbc37498f1be79ef68f8a01e025dfa7c83203c9e50f8eebef8b7108cacba1691",
-        "0xd162905fccb0332a99b5f5f00172a11d1e5c3e7feb9590165b02e19e5f7ba8b5",
-        "0xdd489edccfba1f64d02fd693c22d5faa069d6d3bb65b33b24e5a5d12f99dadce",
-        "0xf5426d21d861dc22aef1b78cb7ade67793ff1caa2d2004324954855c203a48bc",
-    ];
+    let vaultIndex = "1";
+    let coins = ["0x490c9ea233152eeb5d4c6285fb4b94a28110b783670e38ac0f00d7d719972410"];
     let depositAmount = "100000000";
-    let transactionBlock = await getDepositTx(gasBudget, PORTFOLIO_PACKAGE, MODULE, REGISTRY, typeArguments, vaultIndex, coins, depositAmount);
+    let transactionBlock = await getDepositTx(GAS_BUDGET, PORTFOLIO_PACKAGE, MODULE, REGISTRY, typeArguments, vaultIndex, coins, depositAmount);
     return transactionBlock;
 };
 
@@ -103,4 +92,11 @@ const sponsorTransactionE2E = async () => {
     console.log("Execution Status:", executeResponse.effects?.status.status);
 };
 
+const normalTransactionE2E = async () => {
+    const transactionBlock = await progTxnTransfer();
+    let res = await signer.signAndExecuteTransactionBlock({ transactionBlock });
+    console.log(res);
+};
+
 sponsorTransactionE2E();
+// normalTransactionE2E();
