9cf614fe457e32ddee8d06e24eb39d88102fe594
jericlong <jeric.l@alphabit.software>
2023-04-28 17:37:25 +0800

    move leader-board.ts, mint.ts

---
+++ utils/portfolio/mint.ts
@@ -0,0 +1,16 @@
+import { TransactionBlock } from "@mysten/sui.js";
+
+export async function getMintTx(gasBudget: number, packageId: string, registry: string, moduleName: string, amount: number) {
+    const tx = new TransactionBlock();
+    tx.moveCall({
+        target: `${packageId}::${moduleName}::mint`,
+        arguments: [tx.pure(registry), tx.pure(amount)],
+    });
+    tx.setGasBudget(gasBudget);
+
+    return tx;
+}
+
+/*
+sui client call --gas-budget 10000 --package 0xe879e96fb90a8559127091f1176a2d10bb67c6ff --module "token_btc" --function "mint" --args 0x5dce92f67eeda471369b734deac0d540b751cea6  10000
+*/
