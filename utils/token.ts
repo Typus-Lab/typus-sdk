9e584281accfffee87fb5bc58490559667995cd7
Wayne Kuo <wayne.k@typuslab.com>
2023-07-04 11:39:26 +0900

    fix testnet assetToDecimal

--- utils/token.ts
+++ utils/token.ts
@@ -5,9 +5,9 @@ export function typeArgsToAssets(typeArgs: string[]): string[] {
         let typeArgs = x.split("::");
         switch (normalizeSuiAddress(typeArgs[0])) {
             case "0x027792d9fed7f9844eb4839566001bb6f6cb4804f66aa2da6fe1ee242d896881":
-                return "WBTC";
+                return "BTC";
             case "0xaf8cd5edc19c4512f4259f0bee101a40d41ebed738ade5874359610ef8eeced5":
-                return "WETH";
+                return "ETH";
             case "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf":
                 return "USDC";
             case "0xc060006111016b8a020ad5b33834984a437aaa7d3c74c18e09a95d48aceab08c":
@@ -23,9 +23,9 @@ export function assetToDecimal(asset: string): number | undefined {
     switch (asset) {
         case "SUI":
             return 9;
-        case "WBTC":
+        case "BTC":
             return 8;
-        case "WETH":
+        case "ETH":
             return 8;
         case "USDC":
             return 6;
