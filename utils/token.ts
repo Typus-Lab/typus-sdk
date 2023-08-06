f4e6aee501fdb1412aa27596368a1f6784c258c2
Chitaolang <chitaolang@pm.me>
2023-08-06 18:14:52 +0800

    ✨ feat(token name): remove wrapped name for mainnet

--- utils/token.ts
+++ utils/token.ts
@@ -6,20 +6,20 @@ export function typeArgsToAssets(typeArgs: string[]): string[] {
         let typeArgs = x.split("::");
         switch (normalizeSuiAddress(typeArgs[0])) {
             case "0x027792d9fed7f9844eb4839566001bb6f6cb4804f66aa2da6fe1ee242d896881":
-                return "WBTC";
+                return "BTC";
             case "0xaf8cd5edc19c4512f4259f0bee101a40d41ebed738ade5874359610ef8eeced5":
-                return "WETH";
+                return "ETH";
             case "0xb7844e289a8410e50fb3ca48d69eb9cf29e27d223ef90353fe1bd8e27ff8f3f8":
-                return "WSOL";
+                return "SOL";
             case "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf":
                 return "USDC";
             case "0xc060006111016b8a020ad5b33834984a437aaa7d3c74c18e09a95d48aceab08c":
                 return "USDT";
             case config.TOKEN_PACKAGE:
                 if (typeArgs[2] == "BTC") {
-                    return "WBTC";
+                    return "BTC";
                 } else if (typeArgs[2] == "ETH") {
-                    return "WETH";
+                    return "ETH";
                 }
 
                 return typeArgs[2];
