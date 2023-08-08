d6173f0917a97884ac260a0c7dbd413b6950c94c
Chitaolang <chitaolang@pm.me>
2023-08-08 14:59:28 +0800

    ✨ feat(parse token name): add wrapped name back

--- utils/token.ts
+++ utils/token.ts
@@ -6,9 +6,9 @@ export function typeArgsToAssets(typeArgs: string[]): string[] {
         let typeArgs = x.split("::");
         switch (normalizeSuiAddress(typeArgs[0])) {
             case "0x027792d9fed7f9844eb4839566001bb6f6cb4804f66aa2da6fe1ee242d896881":
-                return "BTC";
+                return "WBTC";
             case "0xaf8cd5edc19c4512f4259f0bee101a40d41ebed738ade5874359610ef8eeced5":
-                return "ETH";
+                return "WETH";
             case "0xb7844e289a8410e50fb3ca48d69eb9cf29e27d223ef90353fe1bd8e27ff8f3f8":
                 return "SOL";
             case "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf":
@@ -17,9 +17,9 @@ export function typeArgsToAssets(typeArgs: string[]): string[] {
                 return "USDT";
             case config.TOKEN_PACKAGE:
                 if (typeArgs[2] == "BTC") {
-                    return "BTC";
+                    return "WBTC";
                 } else if (typeArgs[2] == "ETH") {
-                    return "ETH";
+                    return "WETH";
                 }
 
                 return typeArgs[2];
