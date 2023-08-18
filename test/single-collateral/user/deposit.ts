0af54bf44e7aff42f922c534b9f5f2e7d927e216
Wayne Kuo <wayne.k@typuslab.com>
2023-08-19 01:47:39 +0800

    Update deposit.ts

--- test/single-collateral/user/deposit.ts
+++ test/single-collateral/user/deposit.ts
@@ -19,14 +19,15 @@ const signer = new RawSigner(keypair, provider);
         config.SINGLE_COLLATERAL_DEPOSIT_VAULT_REGISTRY,
         config.SINGLE_COLLATERAL_BID_VAULT_REGISTRY
     );
-    let coins = (await provider.getCoins({ owner: await signer.getAddress(), coinType: portfolioVaults[index].depositVault.token })).data.map(
-        (coin) => coin.coinObjectId
-    );
+    let coins = (
+        await provider.getCoins({ owner: await signer.getAddress(), coinType: portfolioVaults[index].depositVault.token })
+    ).data.map((coin) => coin.coinObjectId);
     let transactionBlock = await getDepositTx(
         gasBudget,
         config.SINGLE_COLLATERAL_PACKAGE,
         portfolioVaults[index].typeArgs,
         config.SINGLE_COLLATERAL_REGISTRY,
+        config.SINGLE_COLLATERAL_AC_REGISTRY,
         portfolioVaults[index].info.index,
         coins,
         depositAmount
