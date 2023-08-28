f2c3986eb29046c308ca62f243ed01ab24a4424e
Wayne Kuo <wayne.k@typuslab.com>
2023-08-28 12:23:38 +0800

    update BidPaid
    subtract incentive_value

--- utils/typus-dov-single/user-history.ts
+++ utils/typus-dov-single/user-history.ts
@@ -69,17 +69,23 @@ async function parseTxHistory(datas: Array<any>, originPackage: string): Promise
 
                 const parsedJson = event.parsedJson!;
 
-                txHistory.push({
-                    Vault: parsedJson.index,
-                    Action: action,
-                    Date: new Date(Number(event.timestampMs)),
-                    txDigest: event.id.txDigest,
-                    Asset: asset,
-                    Amount: Number(parsedJson.amount) / 10 ** assetToDecimal(asset)!,
-                    Fee: Number(parsedJson.fee) / 10 ** assetToDecimal(asset)!,
-                    BidSize: Number(parsedJson.size) / 10 ** assetToDecimal(asset)!,
-                    BidPaid: Number(parsedJson.coin_value) / 10 ** assetToDecimal(b_asset)!,
-                });
+                if (action == "IncentiveNewBid") {
+                    let i = txHistory.findIndex((x) => x.txDigest == event.id.txDigest);
+                    let incentive_value = Number(parsedJson.incentive_value) / 10 ** assetToDecimal(asset)!;
+                    txHistory[i].BidPaid = txHistory[i].BidPaid - incentive_value;
+                } else {
+                    txHistory.push({
+                        Vault: parsedJson.index,
+                        Action: action,
+                        Date: new Date(Number(event.timestampMs)),
+                        txDigest: event.id.txDigest,
+                        Asset: asset,
+                        Amount: Number(parsedJson.amount) / 10 ** assetToDecimal(asset)!,
+                        Fee: Number(parsedJson.fee) / 10 ** assetToDecimal(asset)!,
+                        BidSize: Number(parsedJson.size) / 10 ** assetToDecimal(asset)!,
+                        BidPaid: Number(parsedJson.coin_value) / 10 ** assetToDecimal(b_asset)!,
+                    });
+                }
 
                 return txHistory;
             }
