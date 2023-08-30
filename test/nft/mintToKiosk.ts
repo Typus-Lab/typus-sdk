baa98fa6a41cc6ef1b767b5c39533ac570a03184
Wayne Kuo <wayne.k@typuslab.com>
2023-08-30 18:15:02 +0800

    update nft

--- test/nft/mintToKiosk.ts
+++ test/nft/mintToKiosk.ts
@@ -1,33 +1,59 @@
-import "../../load_env";
-import { SuiClient } from "@mysten/sui.js/client";
+import "../load_env";
 import config from "../../nft_config.json";
-import { getMintTx } from "../../utils/typus-nft/user-entry";
+import { getMintToKioskTx } from "../../utils/typus-nft/user-entry";
+import { getPool } from "../../utils/typus-nft/fetch";
+import { JsonRpcProvider, Ed25519Keypair, RawSigner, Connection } from "@mysten/sui.js";
 import { getOwnedKiosks } from "@mysten/kiosk";
-import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
 
 const keypair = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
-const client = new SuiClient({ url: config.RPC_ENDPOINT });
+// const client = new SuiClient({ url: config.RPC_ENDPOINT });
+const provider = new JsonRpcProvider(new Connection({ fullnode: config.RPC_ENDPOINT }));
+const signer = new RawSigner(keypair, provider);
+
 const gasBudget = 100000000;
-const address = keypair.toSuiAddress();
-const pool = "";
+// const address = keypair.toSuiAddress();
+const necklace = "typus";
 
 (async () => {
-    const kiosk = await getOwnedKiosks(client, address);
+    const pool = config[necklace];
 
-    const objs = await client.getOwnedObjects({
+    const address = await signer.getAddress();
+
+    console.log(address);
+
+    const objs = await provider.getOwnedObjects({
         owner: address,
         options: { showType: true, showContent: true },
     });
+    // console.log(objs);
+
     const wlTokens = objs.data.filter(
         (obj) =>
             // @ts-ignore
             obj.data?.type?.startsWith(config.PACKAGE) && obj.data?.content?.fields.for == pool
     );
-    const wlToken = wlTokens[0].data?.objectId!;
 
-    let transactionBlock = await getMintTx(gasBudget, config.PACKAGE, pool, wlToken);
+    // console.log(wlTokens);
 
-    let res = await client.signAndExecuteTransactionBlock({ signer: keypair, transactionBlock });
+    const poolData = await getPool(provider, pool);
 
-    console.log(res);
+    console.log(poolData);
+
+    const kiosks = await getOwnedKiosks(provider, address);
+
+    if (wlTokens.length > 0) {
+        const wlToken = wlTokens[0].data?.objectId!;
+
+        const kiosk = kiosks.kioskIds[0];
+        const kioskOwnerCap = kiosks.kioskOwnerCaps[0];
+
+        if (kioskOwnerCap.kioskId == kiosk) {
+            let transactionBlock = await getMintToKioskTx(gasBudget, config.PACKAGE, pool, wlToken, kiosk, kioskOwnerCap.objectId);
+
+            // let res = await client.signAndExecuteTransactionBlock({ signer: keypair, transactionBlock });
+            let res = await signer.signAndExecuteTransactionBlock({ transactionBlock });
+
+            console.log(res);
+        }
+    }
 })();
