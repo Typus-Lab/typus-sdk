9d42c14e243ce857a99f0f5a9e1b42b7b48235b4
jericlong <jeric.l@alphabit.software>
2023-11-22 11:08:20 +0800

    update mainnet package

--- test/typus-dov-single-v2/user-entry/newBid.ts
+++ test/typus-dov-single-v2/user-entry/newBid.ts
@@ -8,18 +8,18 @@ const keypair = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
 const signer = new RawSigner(keypair, provider);
 
 (async () => {
-    let depositToken = "0x949572061c09bbedef3ac4ffc42e58632291616f0605117cec86d840e09bf519::btc::BTC";
-    let bidToken = "0x949572061c09bbedef3ac4ffc42e58632291616f0605117cec86d840e09bf519::btc::BTC";
+    let depositToken = "0x2::sui::SUI";
+    let bidToken = "0x2::sui::SUI";
     let gasBudget = 100000000;
     let packageId = config.PACKAGE;
     let typeArguments = [depositToken, bidToken];
     let registry = config.REGISTRY;
-    let index = "3";
+    let index = "1";
     let coins = (await provider.getCoins({ owner: await signer.getAddress(), coinType: depositToken })).data.map(
         (coin) => coin.coinObjectId
     );
-    let size = "4";
-    let premium_required = "100000000";
+    let size = "1000000000";
+    let premium_required = "1000000000";
 
     let transactionBlock = await getNewBidTx(gasBudget, packageId, typeArguments, registry, index, coins, size, premium_required);
     let res = await signer.signAndExecuteTransactionBlock({ transactionBlock });
