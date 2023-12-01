253d701e589f67632191623ebdcd4d26a4c288be
Wayne Kuo <wayne.k@typuslab.com>
2023-12-01 09:57:33 +0800

    Update getVaults.ts

--- test/typus-dov-single-v2/view-function/getVaults.ts
+++ test/typus-dov-single-v2/view-function/getVaults.ts
@@ -1,10 +1,13 @@
-import { JsonRpcProvider, Connection } from "@mysten/sui.js";
+import config from "../../../config_v2.json";
+import { SuiClient } from "@mysten/sui.js/client";
 import { getVaults } from "../../../utils/typus-dov-single-v2/view-function";
-import config from "../config.json";
 
-const provider = new JsonRpcProvider(new Connection({ fullnode: config.RPC_ENDPOINT }));
+const provider = new SuiClient({
+    url: config.RPC_ENDPOINT,
+});
+
 (async () => {
     let indexes = [];
-    let result = await getVaults(provider, config.PACKAGE, config.REGISTRY, indexes);
+    let result = await getVaults(provider, config.SINGLE_COLLATERAL_PACKAGE, config.SINGLE_COLLATERAL_REGISTRY, indexes);
     console.log(JSON.stringify(result, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2));
 })();
