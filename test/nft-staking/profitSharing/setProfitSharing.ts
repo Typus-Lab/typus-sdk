5db7329cb7f2f9892f341f9a37e54b86ba01ef60
Wayne Kuo <wayne.k@typuslab.com>
2023-12-01 18:20:56 +0800

    Update setProfitSharing.ts

--- test/nft-staking/profitSharing/setProfitSharing.ts
+++ test/nft-staking/profitSharing/setProfitSharing.ts
@@ -1,32 +1,37 @@
-import "../../load_env";
-import config_v2 from "../../../config_v2.json";
-import mnemonic from "../../../mnemonic.json";
-import { JsonRpcProvider, Ed25519Keypair, RawSigner, Connection } from "@mysten/sui.js";
-import { getTails } from "../../../utils/typus-nft/fetch";
+import config from "../../../config_v2.json";
+import { KioskClient, Network } from "@mysten/kiosk";
+import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
+import { getTailsIds, getTails } from "../../../utils/typus-nft/fetch";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { getStakeNftTx } from "../../../utils/nft-staking/user-entry";
 import { getAllocateProfitSharingTx, getSetProfitSharingTx } from "../../../utils/nft-staking/authorized-entry";
+import { getProfitSharing, ProfitSharing, calculateLevelReward } from "../../../utils/tails-exp-dice/fetch";
+
+import mnemonic from "../../../mnemonic.json";
+import { assert } from "console";
 
 const keypair = Ed25519Keypair.deriveKeypair(String(mnemonic.MNEMONIC));
-const provider = new JsonRpcProvider(new Connection({ fullnode: config_v2.RPC_ENDPOINT }));
-const signer = new RawSigner(keypair, provider);
-
+const provider = new SuiClient({
+    url: getFullnodeUrl("testnet"),
+});
 const gasBudget = 100000000;
 
-const levelProfits = [0, 1, 2, 3, 4, 5, 6]; // MIST
+const totalRewards = 36_000000000;
+const levelShares = [0, 0.1, 0.2, 0.3, 0.4, 0, 0];
 
 (async () => {
-    console.assert(levelProfits.length == 7);
-    const address = await signer.getAddress();
+    const address = keypair.toSuiAddress();
     console.log(address);
 
     var result = await provider.getDynamicFields({
-        parentId: config_v2.NFT_TABLE,
+        parentId: config.NFT_TABLE,
     });
 
     var datas = result.data;
 
     while (result.hasNextPage) {
         result = await provider.getDynamicFields({
-            parentId: config_v2.NFT_TABLE,
+            parentId: config.NFT_TABLE,
             cursor: result.nextCursor,
         });
         datas = datas.concat(result.data);
@@ -39,23 +44,31 @@ const levelProfits = [0, 1, 2, 3, 4, 5, 6]; // MIST
     );
     console.log(tails);
 
-    const sum = tails.reduce((sum, tail) => (sum += levelProfits[Number(tail.level) - 1]), 0);
-    console.log("total profit sharing: " + sum);
+    const levelUsers = [0, 0, 0, 0, 0, 0, 0];
+    tails.forEach((tail) => (levelUsers[Number(tail.level) - 1] += 1));
+    console.log("Level Users: ", levelUsers);
 
-    const users = datas.map((d) => d.name.value);
+    const levelProfits = calculateLevelReward(totalRewards, levelShares, levelUsers);
+    console.log("Level Profits: ", levelProfits);
+
+    const sum = levelUsers.reduce((sum, user, i) => (sum += user * levelProfits[i]), 0);
+    console.log("Sum Profits: ", sum);
+    assert(totalRewards > sum);
+
+    const users = datas.map((d) => d.name.value as string);
     console.log("users.length: " + users.length);
 
     // STEP 1
 
     var transactionBlock = await getSetProfitSharingTx(
         gasBudget,
-        config_v2.SINGLE_COLLATERAL_PACKAGE,
-        config_v2.SINGLE_COLLATERAL_REGISTRY,
+        config.SINGLE_COLLATERAL_PACKAGE,
+        config.SINGLE_COLLATERAL_REGISTRY,
         levelProfits,
         sum
     );
 
-    var res = await signer.signAndExecuteTransactionBlock({ transactionBlock });
+    var res = await provider.signAndExecuteTransactionBlock({ signer: keypair, transactionBlock });
     console.log(`getSetProfitSharingTx:`);
     console.log(res);
 
@@ -63,12 +76,12 @@ const levelProfits = [0, 1, 2, 3, 4, 5, 6]; // MIST
 
     var transactionBlock = await getAllocateProfitSharingTx(
         gasBudget,
-        config_v2.SINGLE_COLLATERAL_PACKAGE,
-        config_v2.SINGLE_COLLATERAL_REGISTRY,
+        config.SINGLE_COLLATERAL_PACKAGE,
+        config.SINGLE_COLLATERAL_REGISTRY,
         users
     );
 
-    var res = await signer.signAndExecuteTransactionBlock({ transactionBlock });
+    var res = await provider.signAndExecuteTransactionBlock({ signer: keypair, transactionBlock });
     console.log(`getAllocateProfitSharingTx:`);
     console.log(res);
 })();
