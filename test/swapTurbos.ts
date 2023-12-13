e327b92122f46ecae3eb939f160fe99ff6a49f40
jericlong <jeric.l@alphabit.software>
2023-12-13 23:34:21 +0800

    refactor swapTurbos, swapAftermath

--- test/swapTurbos.ts
+++ test/swapTurbos.ts
@@ -4,7 +4,6 @@ import { Network, TurbosSdk } from "turbos-clmm-sdk";
 import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
 
 // Choose one way
-export const sdk = new TurbosSdk(Network.mainnet);
 const clientMnemonic = String(process.env.CLIENT_MNEMONIC); // 0xfdfc223abec70fccf517642b22ab26f13abadf7650211d56c123d1c941104434
 const typusMnemonic = String(process.env.TYPUS_MNEMONIC); // 0xfa27437f36c319fcfd6ca8a41a365dbe507c4023ff8d463f4bc70683d38b0496
 const mnemonic1 = String(process.env.MNEMONIC1); // 0x603272d1e61de086a18751482684f882642eb080144aee425c4c34b5e56470b0
@@ -13,6 +12,7 @@ const USDC = "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf
 const FUD = "0x76cb819b01abed502bee8a702b4c2d547532c12f25001c9dea795a5e631c26f1::fud::FUD";
 
 (async () => {
+    const sdk = new TurbosSdk(Network.mainnet);
     const pools = await sdk.pool.getPools();
     const targetPools = pools
         .filter((pool) => pool.types.includes("0x2::sui::SUI") && pool.types.includes(FUD))
@@ -21,28 +21,35 @@ const FUD = "0x76cb819b01abed502bee8a702b4c2d547532c12f25001c9dea795a5e631c26f1:
     const fudPool = targetPools[0];
     console.log(JSON.stringify(fudPool));
     await Promise.all([
-        // turbosSwap(FUD, clientMnemonic, "5000000000000", "10"),
-        // turbosSwap(FUD, typusMnemonic, "29800000000000", "10"),
-        // turbosSwap(FUD, mnemonic1, "1000000000", "1"),
-        // turbosSwap(FUD, mnemonic2, "1000000000", "1"),
-        // turbosSwap(FUD, typusMnemonic, "29800000000000", "10"),
+        // swapTurbos(FUD, clientMnemonic, "5000000000000", "10"),
+        // swapTurbos(FUD, typusMnemonic, "29800000000000", "10"),
+        // swapTurbos(USDC, mnemonic1, "1000000000", "1"),
+        // swapTurbos(USDC, mnemonic2, "1000000000", "1"),
     ]);
 })();
 
-export async function turbosSwap(targetCoin, mnemonic, amount, slippage) {
+export async function swapTurbos(targetCoin, mnemonic, amount, slippage) {
     var result = "";
+
+    const sdk = new TurbosSdk(Network.mainnet);
+
     const keypair = Ed25519Keypair.deriveKeypair(mnemonic);
+
     const provider = new SuiClient({
         url: getFullnodeUrl("mainnet"),
     });
+
     const address = keypair.toSuiAddress();
     result += address + "\n";
 
     const pools = await sdk.pool.getPools();
+    result += pools.length + " ";
+
     const targetPools = pools
         .filter((pool) => pool.types.includes("0x2::sui::SUI") && pool.types.includes(targetCoin))
         .sort((a, b) => Number(b.liquidity) - Number(a.liquidity));
-    result += pools.length + " " + targetPools.length + "\n";
+    result += targetPools.length + "\n";
+
     const fudPool = targetPools[0];
     result += JSON.stringify(fudPool) + "\n";
 
