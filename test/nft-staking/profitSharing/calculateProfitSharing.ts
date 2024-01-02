eb176265de02715ec53b42d691bc7ae58ec6681b
Wayne Kuo <wayne.k@typuslab.com>
2024-01-02 16:37:16 +0800

    Update calculateProfitSharing.ts

--- test/nft-staking/profitSharing/calculateProfitSharing.ts
+++ test/nft-staking/profitSharing/calculateProfitSharing.ts
@@ -1,47 +1,47 @@
 import "../../load_env";
-import config_v2 from "../../../config_v2.json";
+import config_v2 from "../../../mainnet.json";
 import { getProfitSharing, ProfitSharing, calculateLevelReward } from "../../../utils/tails-exp-dice/fetch";
 import { getTails } from "../../../utils/typus-nft/fetch";
 import { SuiClient } from "@mysten/sui.js/client";
 
 const provider = new SuiClient({ url: config_v2.RPC_ENDPOINT });
 
-const totalRewards = 1000_000000000;
-const levelShares = [0, 0.02, 0.06, 0.1, 0.14, 0.24, 0.44];
+const totalRewards = 6666666666;
+const levelShares = [0, 0.01, 0.02, 0.05, 0.08, 0.3, 0.54];
 
 (async () => {
     // Estimation
-    // var result = await provider.getDynamicFields({
-    //     parentId: config_v2.NFT_TABLE,
-    // });
+    var result = await provider.getDynamicFields({
+        parentId: config_v2.NFT_TABLE,
+    });
 
-    // var datas = result.data;
+    var datas = result.data;
 
-    // while (result.hasNextPage) {
-    //     result = await provider.getDynamicFields({
-    //         parentId: config_v2.NFT_TABLE,
-    //         cursor: result.nextCursor,
-    //     });
-    //     datas = datas.concat(result.data);
-    // }
-    // // console.log(datas);
+    while (result.hasNextPage) {
+        result = await provider.getDynamicFields({
+            parentId: config_v2.NFT_TABLE,
+            cursor: result.nextCursor,
+        });
+        datas = datas.concat(result.data);
+    }
+    // console.log(datas);
 
-    // const tails = await getTails(
-    //     provider,
-    //     datas.map((data) => data.objectId)
-    // );
-    // // console.log(tails);
+    const tails = await getTails(
+        provider,
+        datas.map((data) => data.objectId)
+    );
+    // console.log(tails);
 
-    // const users = datas.map((d) => d.name.value);
-    // console.log("users.length: " + users.length);
+    const users = datas.map((d) => d.name.value);
+    console.log("users.length: " + users.length);
 
-    // console.log("total profit sharing: " + totalRewards);
+    console.log("total profit sharing: " + totalRewards);
 
-    var level_users = [106, 108, 25, 1, 1, 1, 0];
-    // tails.forEach((tail) => (level_users[Number(tail.level) - 1] += 1));
+    var level_users = [0, 0, 0, 0, 0, 0, 0];
+    tails.forEach((tail) => (level_users[Number(tail.level) - 1] += 1));
     console.log("level_users: " + level_users);
 
     // calculation
-    const rewards = calculateLevelReward(totalRewards, levelShares, level_users).map((x) => x / 1000000000);
+    const rewards = calculateLevelReward(totalRewards, levelShares, level_users).map((x) => x);
     console.log("level_profits: ", rewards);
 })();
