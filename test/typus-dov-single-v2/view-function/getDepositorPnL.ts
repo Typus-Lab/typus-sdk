6b24cb75d995354375b6c4da188220e44bfc1c7b
Wayne Kuo <wayne.k@typuslab.com>
2024-02-22 11:10:40 +0800

    getDepositorPnL

---
+++ test/typus-dov-single-v2/view-function/getDepositorPnL.ts
@@ -0,0 +1,40 @@
+import config from "../../../config_v2.json";
+import { SuiClient } from "@mysten/sui.js/client";
+import { DepositShare, getDepositShares, getVaults } from "../../../utils/typus-dov-single-v2/view-function";
+import { getUserHistory } from "../../../utils/typus-dov-single-v2/user-history";
+import { getDepositorCashFlows } from "../../../utils/typus-dov-single-v2/depositorPnl";
+
+const provider = new SuiClient({
+    url: config.RPC_ENDPOINT,
+});
+
+(async () => {
+    const user = "0xd15f079d5f60b8fdfdcf3ca66c0d3473790c758b04b6418929d5d2991c5443ee";
+
+    // userHistory
+    const vaults = await getVaults(provider, config.SINGLE_COLLATERAL_PACKAGE, config.SINGLE_COLLATERAL_REGISTRY, []);
+    const userHistory = await getUserHistory(provider, config.SINGLE_COLLATERAL_PACKAGE_ORIGIN, vaults, user, 0);
+    console.log(userHistory.length);
+
+    // depositorCashFlows
+    const depositorCashFlows = await getDepositorCashFlows(userHistory);
+    console.log(depositorCashFlows);
+
+    //
+})();
+
+// let prices = await getLatestPriceUSD();
+// // console.log(prices);
+
+// let sum = 0;
+// for (let [token, amount] of depositorCashFlows.entries()) {
+//     let price = prices.get(token.replace("W", ""))!;
+//     // console.log(`${token} ${price * amount}`);
+//     sum += price * amount;
+// }
+// // console.log(`${user} total earn ${sum} USD from Typus`);
+
+// let res_1 = await getUserStake(provider, config.NFT_TABLE, user);
+// // console.log(res_1!.number); // null
+
+// return [sum, res_1];
---
+++ utils/typus-dov-single-v2/depositorPnl.ts
@@ -0,0 +1,132 @@
+import { TxHistory } from "../../utils/typus-dov-single-v2/user-history";
+
+export async function getDepositorCashFlows(userHistory: TxHistory[]) {
+    let depositorCashFlows = new Map<string, DepositorCashFlow>();
+
+    for (let history of userHistory) {
+        const index = history.Index!;
+
+        if (history.Action.startsWith("Harvest")) {
+            const [amount, token] = history.Amount?.split(" ")!;
+            if (depositorCashFlows.has(index)) {
+                let depositorCashFlow = depositorCashFlows.get(index)!;
+                let totalHarvest = depositorCashFlow.totalHarvest;
+                if (totalHarvest.has(token)) {
+                    let sum = totalHarvest.get(token)!;
+                    totalHarvest.set(token, sum + Number(amount));
+                } else {
+                    totalHarvest.set(token, Number(amount));
+                }
+                depositorCashFlow.totalHarvest = totalHarvest;
+                depositorCashFlows.set(index, depositorCashFlow);
+            } else {
+                let totalHarvest = new Map();
+                totalHarvest.set(token, Number(amount));
+                let depositorCashFlow: DepositorCashFlow = {
+                    D_TOKEN: undefined,
+                    totalDeposit: 0,
+                    totalWithdraw: 0,
+                    totalClaim: 0,
+                    totalCompound: 0,
+                    totalHarvest,
+                };
+                depositorCashFlows.set(index, depositorCashFlow);
+            }
+        } else if (history.Action.startsWith("Deposit")) {
+            const [amount, token] = history.Amount?.split(" ")!;
+            if (depositorCashFlows.has(index)) {
+                let depositorCashFlow = depositorCashFlows.get(index)!;
+                depositorCashFlow.D_TOKEN = token;
+                depositorCashFlow.totalDeposit += Number(amount);
+                depositorCashFlows.set(index, depositorCashFlow);
+            } else {
+                let depositorCashFlow: DepositorCashFlow = {
+                    D_TOKEN: token,
+                    totalDeposit: Number(amount),
+                    totalWithdraw: 0,
+                    totalClaim: 0,
+                    totalCompound: 0,
+                    totalHarvest: new Map(),
+                };
+                depositorCashFlows.set(index, depositorCashFlow);
+            }
+        } else if (history.Action.startsWith("Withdraw")) {
+            const [amount, token] = history.Amount?.split(" ")!;
+            if (depositorCashFlows.has(index)) {
+                let depositorCashFlow = depositorCashFlows.get(index)!;
+                depositorCashFlow.D_TOKEN = token;
+                depositorCashFlow.totalWithdraw += Number(amount);
+                depositorCashFlows.set(index, depositorCashFlow);
+            } else {
+                let depositorCashFlow: DepositorCashFlow = {
+                    D_TOKEN: token,
+                    totalDeposit: 0,
+                    totalWithdraw: Number(amount),
+                    totalClaim: 0,
+                    totalCompound: 0,
+                    totalHarvest: new Map(),
+                };
+                depositorCashFlows.set(index, depositorCashFlow);
+            }
+        } else if (history.Action == "Claim") {
+            const [amount, token] = history.Amount?.split(" ")!;
+            if (depositorCashFlows.has(index)) {
+                let depositorCashFlow = depositorCashFlows.get(index)!;
+                depositorCashFlow.D_TOKEN = token;
+                depositorCashFlow.totalClaim += Number(amount);
+                depositorCashFlows.set(index, depositorCashFlow);
+            } else {
+                let depositorCashFlow: DepositorCashFlow = {
+                    D_TOKEN: token,
+                    totalDeposit: 0,
+                    totalWithdraw: 0,
+                    totalClaim: Number(amount),
+                    totalCompound: 0,
+                    totalHarvest: new Map(),
+                };
+                depositorCashFlows.set(index, depositorCashFlow);
+            }
+        } else if (history.Action == "Compound") {
+            const [amount, token] = history.Amount?.split(" ")!;
+            if (depositorCashFlows.has(index)) {
+                let depositorCashFlow = depositorCashFlows.get(index)!;
+                depositorCashFlow.D_TOKEN = token;
+                depositorCashFlow.totalCompound += Number(amount);
+                depositorCashFlows.set(index, depositorCashFlow);
+            } else {
+                let depositorCashFlow: DepositorCashFlow = {
+                    D_TOKEN: token,
+                    totalDeposit: 0,
+                    totalWithdraw: 0,
+                    totalClaim: 0,
+                    totalCompound: Number(amount),
+                    totalHarvest: new Map(),
+                };
+                depositorCashFlows.set(index, depositorCashFlow);
+            }
+        }
+    }
+    console.log(depositorCashFlows);
+
+    return depositorCashFlows;
+}
+
+interface DepositorCashFlow {
+    D_TOKEN: string | undefined;
+    totalDeposit: number;
+    totalWithdraw: number;
+    totalClaim: number;
+    totalCompound: number;
+    totalHarvest: Map<string, number>;
+}
+
+export async function calculateNetDeposit(depositorCashFlows: Map<string, DepositorCashFlow>) {
+    let netDeposits = new Map<string, number>();
+
+    for (let [index, share] of depositorCashFlows.entries()) {
+        let netDeposit = share.totalDeposit + share.totalCompound - share.totalWithdraw - share.totalClaim;
+        netDeposits.set(index, netDeposit);
+    }
+
+    return netDeposits;
+}
