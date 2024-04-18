ddf77955eebebb4031ef06de7f73ad9611d7fbed
jericlong <jeric.l@alphabit.software>
2024-04-18 15:25:44 +0800

    Update config.json

--- test/typus-dov-single-v2/view-function/getVaults.ts
+++ test/typus-dov-single-v2/view-function/getVaults.ts
@@ -2,13 +2,13 @@ import configs from "../config.json";
 import { SuiClient } from "@mysten/sui.js/client";
 import { getVaults } from "../../../utils/typus-dov-single-v2/view-function";
 
-const config = configs.MAINNET;
+const config = configs.TESTNET;
 const provider = new SuiClient({
     url: config.RPC_ENDPOINT,
 });
 
 (async () => {
-    let indexes = ["21"];
-    let result = await getVaults(provider, config.DOV_SINGLE_PACKAGE, config.DOV_SINGLE_REGISTRY, indexes);
+    let indexes = [];
+    let result = await getVaults(provider, config.PACKAGE.DOV_SINGLE_PACKAGE, config.REGISTRY.DOV_SINGLE_REGISTRY, indexes);
     console.log(JSON.stringify(result, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2));
 })();
