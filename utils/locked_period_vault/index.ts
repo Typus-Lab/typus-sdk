549ccede46faa98e1a4cbe785513c2c5e23325ef
Wayne Kuo <wayne.k@typuslab.com>
2024-04-25 14:12:39 +0800

    Update index.ts

--- utils/locked_period_vault/index.ts
+++ utils/locked_period_vault/index.ts
@@ -1,2 +1,2 @@
-export const PACKAGE_ID = "0x0";
+export const PACKAGE_ID = "0x715ff0148b901aa0e709b23c9b9eb2fde52325c868fe4bce0db9fff408608ad8";
 export const PUBLISHED_AT = "0x715ff0148b901aa0e709b23c9b9eb2fde52325c868fe4bce0db9fff408608ad8";
