ed39f3be5ec9eda93b8f81e897b6dd4d59694634
Wayne Kuo <wayne.k@typuslab.com>
2024-04-26 10:28:34 +0800

    add locked-period-vault tests

---
+++ test/locked-period-vault/user/leave.ts
@@ -0,0 +1,34 @@
+import configs from "../../../config.json";
+import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { depositAndLockReceipt, leave } from "../../../utils/locked-period-vault/locked-period-vault/functions";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { CLOCK } from "../../../constants";
+import "../../load_env";
+
+const keypair = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
+
+const config = configs.TESTNET;
+
+const provider = new SuiClient({
+    url: config.RPC_ENDPOINT,
+});
+const gasBudget = 100000000;
+
+(async () => {
+    const user = keypair.toSuiAddress();
+    console.log(user);
+
+    let tx = new TransactionBlock();
+    tx.setGasBudget(gasBudget);
+
+    leave(tx, {
+        lockedVaultRegistry: config.REGISTRY.LOCKED_VAULT,
+        index: BigInt(0), // 0 Sui Hourly Call, 9 Sui Hourly Put
+        clock: CLOCK,
+    });
+
+    let res = await provider.signAndExecuteTransactionBlock({ signer: keypair, transactionBlock: tx });
+
+    console.log(res);
+})();
