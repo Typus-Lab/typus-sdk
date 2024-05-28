79ad5819f0d42d7e124a647a65b153da9ae60c07
jericlong <jeric.l@alphabit.software>
2024-05-28 10:06:11 +0800

    getUnstakeTailsTx

--- utils/tails-staking/user-entry.ts
+++ utils/tails-staking/user-entry.ts
@@ -91,3 +91,60 @@ export async function getStakeTailsTx(input: {
 
     return input.tx;
 }
+
+/**
+    public fun unstake_tails(
+        version: &mut Version,
+        tails_staking_registry: &mut TailsStakingRegistry,
+        kiosk: &mut Kiosk,
+        kiosk_owner_cap: &KioskOwnerCap,
+        tails: address,
+        ctx: &TxContext,
+    ) {
+*/
+export async function getUnstakeTailsTx(input: {
+    tx: TransactionBlock;
+    typusPackageId: string;
+    typusEcosystemVersion: string;
+    typusTailsStakingRegistry: string;
+    kiosk: string;
+    kioskCap: string;
+    tails: string;
+    personalKioskPackageId: string | undefined;
+}) {
+    if (input.personalKioskPackageId) {
+        const [personalKioskCap, borrow] = input.tx.moveCall({
+            target: `${input.personalKioskPackageId}::personal_kiosk::borrow_val`,
+            arguments: [input.tx.object(input.kioskCap)],
+        });
+        input.tx.moveCall({
+            target: `${input.typusPackageId}::tails_staking::unstake_tails`,
+            typeArguments: [],
+            arguments: [
+                input.tx.object(input.typusEcosystemVersion),
+                input.tx.object(input.typusTailsStakingRegistry),
+                input.tx.object(input.kiosk),
+                personalKioskCap,
+                input.tx.pure(input.tails),
+            ],
+        });
+        input.tx.moveCall({
+            target: `${input.personalKioskPackageId}::personal_kiosk::return_val`,
+            arguments: [input.tx.object(input.kioskCap), personalKioskCap, borrow],
+        });
+    } else {
+        input.tx.moveCall({
+            target: `${input.typusPackageId}::tails_staking::unstake_tails`,
+            typeArguments: [],
+            arguments: [
+                input.tx.object(input.typusEcosystemVersion),
+                input.tx.object(input.typusTailsStakingRegistry),
+                input.tx.object(input.kiosk),
+                input.tx.object(input.kioskCap),
+                input.tx.pure(input.tails),
+            ],
+        });
+    }
+
+    return input.tx;
+}
