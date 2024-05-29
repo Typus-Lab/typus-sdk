2df191194e1614251dee1fe4dba403f4909261a2
jericlong <jeric.l@alphabit.software>
2024-05-29 10:17:14 +0800

    getRemoveIdsTx

--- utils/tails-staking/authorized-entry.ts
+++ utils/tails-staking/authorized-entry.ts
@@ -28,3 +28,31 @@ export async function getUploadIdsTx(input: {
 
     return input.tx;
 }
+
+/**
+    entry fun remove_ids(
+        version: &Version,
+        tails_staking_registry: &mut TailsStakingRegistry,
+        mut count: u64,
+        ctx: &TxContext,
+    ) {
+*/
+export async function getRemoveIdsTx(input: {
+    tx: TransactionBlock;
+    typusPackageId: string;
+    typusEcosystemVersion: string;
+    typusTailsStakingRegistry: string;
+    count: string;
+}) {
+    let result = input.tx.moveCall({
+        target: `${input.typusPackageId}::tails_staking::remove_ids`,
+        typeArguments: [],
+        arguments: [
+            input.tx.object(input.typusEcosystemVersion),
+            input.tx.object(input.typusTailsStakingRegistry),
+            input.tx.pure(input.count),
+        ],
+    });
+
+    return input.tx;
+}
