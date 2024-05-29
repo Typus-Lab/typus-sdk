f4e2dd419bb765bb1ea1f1e0d2305c858ae9bb78
jericlong <jeric.l@alphabit.software>
2024-05-29 10:07:57 +0800

    getStakingInfo

--- utils/tails-staking/view-function.ts
+++ utils/tails-staking/view-function.ts
@@ -1,62 +1,48 @@
-// import { TransactionBlock } from "@mysten/sui.js/transactions";
-// import { SuiClient } from "@mysten/sui.js/client";
-// import { BcsReader } from "@mysten/bcs";
-// import { AddressFromBytes } from "../tools";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { SuiClient } from "@mysten/sui.js/client";
+import { BcsReader } from "@mysten/bcs";
+import { AddressFromBytes } from "../tools";
 
-// export const SENDER = "0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-// export interface Rankings {
-//     user_score: string;
-//     leaderboard: Ranks[];
-// }
-// export interface Ranks {
-//     score: string;
-//     users: string[];
-// }
-// export async function getRankings(input: {
-//     provider: SuiClient;
-//     typusPackageId: string;
-//     typusEcosystemVersion: string;
-//     typusLeaderboardRegistry: string;
-//     key: string;
-//     id: string;
-//     ranks: number;
-//     user: string;
-//     active: boolean;
-// }): Promise<Rankings> {
-//     let transactionBlock = new TransactionBlock();
-//     transactionBlock.moveCall({
-//         target: `${input.typusPackageId}::leaderboard::get_rankings`,
-//         typeArguments: [],
-//         arguments: [
-//             transactionBlock.pure(input.typusEcosystemVersion),
-//             transactionBlock.pure(input.typusLeaderboardRegistry),
-//             transactionBlock.pure(input.key),
-//             transactionBlock.pure(input.id),
-//             transactionBlock.pure(input.ranks),
-//             transactionBlock.pure(input.user),
-//             transactionBlock.pure(input.active),
-//         ],
-//     });
-//     let results = (await input.provider.devInspectTransactionBlock({ sender: SENDER, transactionBlock })).results;
-//     // @ts-ignore
-//     let bytes = results[results.length - 1].returnValues[0][0];
-//     let reader = new BcsReader(new Uint8Array(bytes));
-//     let result = reader.readVec((reader, i) => {
-//         if (i == 0) {
-//             reader.readULEB();
-//             return reader.read64();
-//         } else {
-//             reader.readULEB();
-//             return {
-//                 score: reader.read64(),
-//                 users: reader.readVec((reader) => {
-//                     return AddressFromBytes(reader.readBytes(32));
-//                 }),
-//             };
-//         }
-//     });
-//     return {
-//         user_score: result[0],
-//         leaderboard: result.slice(1),
-//     } as Rankings;
-// }
+export const SENDER = "0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+export interface StakingInfo {
+    user: string;
+    tails: string[];
+    profits: string[];
+    u64Padding: string[];
+}
+export async function getStakingInfo(input: {
+    provider: SuiClient;
+    typusPackageId: string;
+    typusEcosystemVersion: string;
+    typusTailsStakingRegistry: string;
+    user: string;
+}): Promise<StakingInfo> {
+    let transactionBlock = new TransactionBlock();
+    transactionBlock.moveCall({
+        target: `${input.typusPackageId}::tails_staking::get_staking_info`,
+        typeArguments: [],
+        arguments: [
+            transactionBlock.pure(input.typusEcosystemVersion),
+            transactionBlock.pure(input.typusTailsStakingRegistry),
+            transactionBlock.pure(input.user),
+        ],
+    });
+    let results = (await input.provider.devInspectTransactionBlock({ sender: SENDER, transactionBlock })).results;
+    // @ts-ignore
+    let bytes = results[results.length - 1].returnValues[0][0];
+    let reader = new BcsReader(new Uint8Array(bytes));
+    reader.readULEB();
+    return {
+        user: AddressFromBytes(reader.readBytes(32)),
+        tails: reader.readVec((reader) => {
+            return reader.read64();
+        }),
+        profits: reader.readVec((reader) => {
+            return reader.read64();
+        }),
+        u64Padding: reader.readVec((reader) => {
+            return reader.read64();
+        }),
+    } as StakingInfo;
+}
