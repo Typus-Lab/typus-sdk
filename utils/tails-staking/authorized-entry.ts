3399fa07533de8b728dc79b8ca4da1bd16dbdab1
jericlong <jeric.l@alphabit.software>
2024-05-29 10:18:46 +0800

    getUploadLevelsTx, getRemoveLevelsTx

--- utils/tails-staking/authorized-entry.ts
+++ utils/tails-staking/authorized-entry.ts
@@ -56,3 +56,58 @@ export async function getRemoveIdsTx(input: {
 
     return input.tx;
 }
+/**
+    entry fun upload_levels(
+        version: &Version,
+        tails_staking_registry: &mut TailsStakingRegistry,
+        mut count: u64,
+        ctx: &TxContext,
+    ) {
+*/
+export async function getUploadLevelsTx(input: {
+    tx: TransactionBlock;
+    typusPackageId: string;
+    typusEcosystemVersion: string;
+    typusTailsStakingRegistry: string;
+    count: string;
+}) {
+    let result = input.tx.moveCall({
+        target: `${input.typusPackageId}::tails_staking::upload_levels`,
+        typeArguments: [],
+        arguments: [
+            input.tx.object(input.typusEcosystemVersion),
+            input.tx.object(input.typusTailsStakingRegistry),
+            input.tx.pure(input.count),
+        ],
+    });
+
+    return input.tx;
+}
+
+/**
+    entry fun remove_levels(
+        version: &Version,
+        tails_staking_registry: &mut TailsStakingRegistry,
+        mut count: u64,
+        ctx: &TxContext,
+    ) {
+*/
+export async function getRemoveLevelsTx(input: {
+    tx: TransactionBlock;
+    typusPackageId: string;
+    typusEcosystemVersion: string;
+    typusTailsStakingRegistry: string;
+    count: string;
+}) {
+    let result = input.tx.moveCall({
+        target: `${input.typusPackageId}::tails_staking::remove_levels`,
+        typeArguments: [],
+        arguments: [
+            input.tx.object(input.typusEcosystemVersion),
+            input.tx.object(input.typusTailsStakingRegistry),
+            input.tx.pure(input.count),
+        ],
+    });
+
+    return input.tx;
+}
