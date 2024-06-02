1fc8f0050c5a4f8d6612d067ef2c397ff60390ab
jericlong <jeric.l@alphabit.software>
2024-06-02 23:00:46 +0800

    initTailsStakingRegistry

---
+++ scripts/migrateTailsStaking/initTailsStakingRegistry.ts
@@ -0,0 +1,37 @@
+import "../../load_env";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { SuiClient } from "@mysten/sui.js/client";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import configs from "../../config.json";
+import { CLOCK } from "../../constants";
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const config = configs.TESTNET;
+const signer = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
+const provider = new SuiClient({ url: config.RPC_ENDPOINT });
+const tailsManagerCap = "";
+const transferPolicy = "";
+
+(async () => {
+    let transactionBlock = new TransactionBlock();
+    // step 1
+    let result = transactionBlock.moveCall({
+        target: `${config.PACKAGE.DOV_SINGLE}::tails_staking::remove_nft_extension`,
+        typeArguments: [],
+        arguments: [transactionBlock.object(config.REGISTRY.DOV_SINGLE), transactionBlock.object(CLOCK)],
+    });
+    transactionBlock.transferObjects([result[0], result[1], result[2], result[3]], signer.toSuiAddress());
+    // setp 2
+    // transactionBlock.moveCall({
+    //     target: `${config.PACKAGE.TYPUS}::tails_staking::init_tails_staking_registry`,
+    //     typeArguments: [],
+    //     arguments: [
+    //         transactionBlock.object(config.TYPUS_VERSION),
+    //         transactionBlock.object(tailsManagerCap),
+    //         transactionBlock.object(transferPolicy),
+    //     ],
+    // });
+    transactionBlock.setGasBudget(100000000);
+    let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
+    console.log(res);
+})();
