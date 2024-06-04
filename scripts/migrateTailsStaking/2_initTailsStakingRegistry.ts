612c47cbcee544e19ccd26fdc84e8bef7952ef1c
jericlong <jeric.l@alphabit.software>
2024-06-04 16:47:20 +0800

    upload_ids upload_levels

--- scripts/migrateTailsStaking/2_initTailsStakingRegistry.ts
+++ scripts/migrateTailsStaking/2_initTailsStakingRegistry.ts
@@ -11,27 +11,64 @@ const signer = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
 const provider = new SuiClient({ url: config.RPC_ENDPOINT });
 const tailsManagerCap = "";
 const transferPolicy = "";
+const tailsStakingRegistry = config.REGISTRY.TAILS_STAKING;
 
 (async () => {
+    await step1();
+    // await step2();
+    // await step3();
+    // await step4();
+})();
+
+async function step1() {
     let transactionBlock = new TransactionBlock();
-    // step 1
     let result = transactionBlock.moveCall({
         target: `${config.PACKAGE.DOV_SINGLE}::tails_staking::remove_nft_extension`,
         typeArguments: [],
         arguments: [transactionBlock.object(config.REGISTRY.DOV_SINGLE), transactionBlock.object(CLOCK)],
     });
     transactionBlock.transferObjects([result[0], result[1], result[2], result[3]], signer.toSuiAddress());
-    // setp 2
-    // transactionBlock.moveCall({
-    //     target: `${config.PACKAGE.TYPUS}::tails_staking::init_tails_staking_registry`,
-    //     typeArguments: [],
-    //     arguments: [
-    //         transactionBlock.object(config.TYPUS_VERSION),
-    //         transactionBlock.object(tailsManagerCap),
-    //         transactionBlock.object(transferPolicy),
-    //     ],
-    // });
     transactionBlock.setGasBudget(100000000);
     let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
     console.log(res);
-})();
+}
+
+async function step2() {
+    let transactionBlock = new TransactionBlock();
+    transactionBlock.moveCall({
+        target: `${config.PACKAGE.TYPUS}::tails_staking::init_tails_staking_registry`,
+        typeArguments: [],
+        arguments: [
+            transactionBlock.object(config.TYPUS_VERSION),
+            transactionBlock.object(tailsManagerCap),
+            transactionBlock.object(transferPolicy),
+        ],
+    });
+    transactionBlock.setGasBudget(100000000);
+    let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
+    console.log(res);
+}
+
+async function step3() {
+    let transactionBlock = new TransactionBlock();
+    transactionBlock.moveCall({
+        target: `${config.PACKAGE.TYPUS}::tails_staking::upload_ids`,
+        typeArguments: [],
+        arguments: [transactionBlock.object(config.TYPUS_VERSION), transactionBlock.object(tailsStakingRegistry)],
+    });
+    transactionBlock.setGasBudget(100000000);
+    let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
+    console.log(res);
+}
+
+async function step4() {
+    let transactionBlock = new TransactionBlock();
+    transactionBlock.moveCall({
+        target: `${config.PACKAGE.TYPUS}::tails_staking::upload_levels`,
+        typeArguments: [],
+        arguments: [transactionBlock.object(config.TYPUS_VERSION), transactionBlock.object(tailsStakingRegistry)],
+    });
+    transactionBlock.setGasBudget(100000000);
+    let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
+    console.log(res);
+}
