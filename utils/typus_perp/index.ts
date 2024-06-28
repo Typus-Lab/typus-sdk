968b589264db3bac972b3347bb045fcc647f6a3c
Wayne Kuo <wayne.k@typuslab.com>
2024-06-28 17:58:01 +0800

    Update index.ts

--- utils/typus_perp/index.ts
+++ utils/typus_perp/index.ts
@@ -1,2 +1,8 @@
-export const PACKAGE_ID = "0x1a05edb0e5e670196de98fbbf544180d129dd4ec11c3c57f742badf0304650d";
-export const PUBLISHED_AT = "0x1a05edb0e5e670196de98fbbf544180d129dd4ec11c3c57f742badf0304650d";
+import path from "path";
+import dotenv from "dotenv";
+// import .env file if exists
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+export const NETWORK = process.env.CLUSTER == "mainnet" ? "mainnet" : "testnet";
+export const PACKAGE_ID = process.env.CLUSTER == "mainnet" ? "" : "0x1a05edb0e5e670196de98fbbf544180d129dd4ec11c3c57f742badf0304650d";
+export const PUBLISHED_AT = process.env.CLUSTER == "mainnet" ? "" : "0x1a05edb0e5e670196de98fbbf544180d129dd4ec11c3c57f742badf0304650d";
