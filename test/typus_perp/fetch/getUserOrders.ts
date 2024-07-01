65f9685b83f4d0c5232edf455036e8a75211e811
Wayne Kuo <wayne.k@typuslab.com>
2024-07-01 15:35:20 +0800

    getUserOrders

--- test/typus_perp/fetch/getUserOrders.ts
+++ test/typus_perp/fetch/getUserOrders.ts
@@ -1,10 +1,8 @@
 import configs from "../../../config.json";
 import { SuiClient } from "@mysten/sui.js/client";
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
-import { getUserOrders } from "../../../utils/typus_perp/trading/functions";
-import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { getUserOrders } from "../../../utils/typus_perp/fetch/getUserOrders";
 import "../../load_env";
-import { readVecOrder } from "../../../utils/typus_perp/readVec";
 
 const keypair = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
 
@@ -13,29 +11,11 @@ const config = configs.TESTNET;
 const provider = new SuiClient({
     url: config.RPC_ENDPOINT,
 });
-const gasBudget = 100000000;
 
 (async () => {
     const user = keypair.toSuiAddress();
     console.log(user);
 
-    let tx = new TransactionBlock();
-    tx.setGasBudget(gasBudget);
-
-    getUserOrders(tx, {
-        version: config.OBJECT.TYPUS_PERP_VERSION,
-        registry: config.REGISTRY.MARKET_REGISTRY,
-        marketIndex: BigInt(0),
-        user,
-    });
-
-    let res = await provider.devInspectTransactionBlock({ sender: user, transactionBlock: tx });
-    // console.log(res);
-
-    // @ts-ignore
-    const returnValues = res.results[0].returnValues[0][0];
-    // console.log(returnValues);
-
-    const orders = readVecOrder(Uint8Array.from(returnValues));
+    const orders = await getUserOrders(provider, config, user);
     console.log(orders);
 })();
