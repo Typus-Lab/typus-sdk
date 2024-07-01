8fb610a5e97001f9b3bdd70552090527aad5848e
Wayne Kuo <wayne.k@typuslab.com>
2024-07-01 14:21:14 +0800

    getLpPools

--- test/typus_perp/fetch/getLpPools.ts
+++ test/typus_perp/fetch/getLpPools.ts
@@ -1,10 +1,6 @@
 import configs from "../../../config.json";
-import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
-import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
-import { LiquidityPool, Registry } from "../../../utils/typus_perp/lp-pool/structs";
-
-import mnemonic from "../../../mnemonic.json";
-const keypair = Ed25519Keypair.deriveKeypair(String(mnemonic.MNEMONIC));
+import { SuiClient } from "@mysten/sui.js/client";
+import { getLpPools } from "../../../utils/typus_perp/fetch/getLpPools";
 
 const config = configs.TESTNET;
 
@@ -13,18 +9,6 @@ const provider = new SuiClient({
 });
 
 (async () => {
-    const address = keypair.toSuiAddress();
-    console.log(address);
-
-    const lpPoolRegistry = await Registry.fetch(provider, config.REGISTRY.LP_POOL_REGISTRY);
-    console.log(lpPoolRegistry);
-
-    const dynamicFields = await provider.getDynamicFields({
-        parentId: lpPoolRegistry.liquidityPoolRegistry,
-    });
-
-    for (const field of dynamicFields.data) {
-        const lpPool = await LiquidityPool.fetch(provider, field.objectId);
-        console.log(lpPool);
-    }
+    const lpPools = await getLpPools(provider, config);
+    console.log(lpPools);
 })();
