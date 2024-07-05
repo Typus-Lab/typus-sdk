e1a96cb99df17030d5a885d68dd5332a60d021c3
Wayne Kuo <wayne.k@typuslab.com>
2024-07-05 14:16:13 +0800

    update config

--- utils/typus_perp/index.ts
+++ utils/typus_perp/index.ts
@@ -1,8 +1,9 @@
+import configs from "../../config.json";
 import path from "path";
 import dotenv from "dotenv";
 // import .env file if exists
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export const NETWORK = process.env.CLUSTER == "mainnet" ? "MAINNET" : "TESTNET";
-export const PACKAGE_ID = process.env.CLUSTER == "mainnet" ? "" : "0x4bcf9eade4480bcb9fcd3139ec8d22afda34b25af06092904fcccb06e1b8043c";
-export const PUBLISHED_AT = process.env.CLUSTER == "mainnet" ? "" : "0x4bcf9eade4480bcb9fcd3139ec8d22afda34b25af06092904fcccb06e1b8043c";
+export const PACKAGE_ID = process.env.CLUSTER == "mainnet" ? configs.MAINNET.PACKAGE_ORIGIN.PERP : configs.TESTNET.PACKAGE.PERP;
+export const PUBLISHED_AT = process.env.CLUSTER == "mainnet" ? configs.MAINNET.PACKAGE.PERP : configs.TESTNET.PACKAGE.PERP;
