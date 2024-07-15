dd3f29280f1e17b48ad5a790ddb31ef99a26e5f9
Wayne Kuo <wayne.k@typuslab.com>
2024-07-15 15:24:04 +0800

    remove typus-dice

--- test/utils/getDbPrice.ts
+++ test/utils/getDbPrice.ts
@@ -1,4 +1,4 @@
-import { getPairPrices, getLatestPrice } from "../utils/price";
+import { getPairPrices, getLatestPrice } from "../../src";
 
 (async () => {
     const pair = "SUIFUD"; // SUIBUCK, SUIFUD, SUIAFSUI
