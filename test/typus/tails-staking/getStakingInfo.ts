08ede205ab3a2644b63e7f9aa54349993d5f4e77
jericlong <jeric.l@alphabit.software>
2024-08-05 15:32:25 +0800

    fix ts-node require

--- test/typus/tails-staking/getStakingInfo.ts
+++ test/typus/tails-staking/getStakingInfo.ts
@@ -2,7 +2,7 @@ import configs from "config.json";
 import { SuiClient } from "@mysten/sui.js/client";
 import { getStakingInfo } from "src/typus/tails-staking";
 
-const config = configs.TESTNET;
+const config = configs.MAINNET;
 const provider = new SuiClient({
     url: config.RPC_ENDPOINT,
 });
@@ -13,7 +13,7 @@ const provider = new SuiClient({
         typusPackageId: config.PACKAGE.TYPUS,
         typusEcosystemVersion: config.OBJECT.TYPUS_VERSION,
         typusTailsStakingRegistry: config.REGISTRY.TAILS_STAKING,
-        user: "0xb6b29d18c728503fb59cc59ecbe52611d26b2746b2cedc8d38cabf81428cae6c",
+        user: "0x603272d1e61de086a18751482684f882642eb080144aee425c4c34b5e56470b0",
     });
     console.log(JSON.stringify(result, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2));
 })();
