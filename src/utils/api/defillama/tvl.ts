223a0023c7a43381c1553b26fa9fc29753aa8041
benson <rhualikescode@gmail.com>
2024-09-19 16:36:35 +0800

    Add dov tvl fn

--- src/utils/api/defillama/tvl.ts
+++ src/utils/api/defillama/tvl.ts
@@ -1,4 +1,16 @@
 export async function getTvl(): Promise<number> {
+    let response = await fetch("https://api.llama.fi/tvl/typus-finance", {
+        method: "GET",
+    });
+
+    // console.log(response);
+    let data = await response.json();
+    // console.log(data);
+
+    return data;
+}
+
+export async function getDovTvl(): Promise<number> {
     let response = await fetch("https://api.llama.fi/tvl/typus-dov", {
         method: "GET",
     });
