b38b24e127ba0e90aba4761d21ef114333a5e76a
Wayne Kuo <wayne.k@typuslab.com>
2024-10-24 11:46:25 +0800

    fix typeArguments

--- test/typus-dov-single-v2/view-function/getRefundShares.ts
+++ test/typus-dov-single-v2/view-function/getRefundShares.ts
@@ -2,15 +2,11 @@ import { TypusConfig } from "src/utils";
 import { getRefundShares } from "src/typus-dov-single-v2";
 
 (async () => {
-    let config = await TypusConfig.default("TESTNET", null);
+    let config = await TypusConfig.default("MAINNET", null);
 
     let user = "0x603272d1e61de086a18751482684f882642eb080144aee425c4c34b5e56470b0";
-    let typeArguments = [
-        "0x2::sui::SUI",
-        "0x949572061c09bbedef3ac4ffc42e58632291616f0605117cec86d840e09bf519::btc::BTC",
-        "0x949572061c09bbedef3ac4ffc42e58632291616f0605117cec86d840e09bf519::eth::ETH",
-        "0x949572061c09bbedef3ac4ffc42e58632291616f0605117cec86d840e09bf519::usdc::USDC",
-    ];
+    let typeArguments = Object.values(config.token).filter((t) => t != "");
+    // console.log(typeArguments);
     let result = await getRefundShares(config, { typeArguments, user });
     console.log(JSON.stringify(result, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2));
 })();
