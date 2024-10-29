66b05701c2363e5eb0ebe6a6fd38d033f6008ced
Wayne Kuo <wayne.k@typuslab.com>
2024-10-29 16:11:01 +0800

    Create addUserShare.ts

---
+++ test/typus-launch/option-airdrop/addUserShare.ts
@@ -0,0 +1,34 @@
+import "src/utils/load_env";
+import { addUserShare } from "src/typus-launch/option-airdrop";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { SuiClient } from "@mysten/sui.js/client";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { TypusConfig } from "src/utils";
+import mnemonic from "mnemonic.json";
+import * as fs from "fs";
+
+(async () => {
+    let config = await TypusConfig.default("TESTNET", null);
+    let signer = Ed25519Keypair.deriveKeypair(String(mnemonic.MNEMONIC));
+    let provider = new SuiClient({ url: config.rpcEndpoint });
+    let user = signer.toSuiAddress();
+    console.log("User address:", user);
+
+    const raw = fs.readFileSync("airdrop.csv", "utf-8");
+    const user_data = raw.split("\n").map((line) => line.split(","));
+    const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    while (user_data.length > 0) {
+        console.log(user_data.length);
+        let slice = user_data.splice(0, 300);
+        let transactionBlock = new TransactionBlock();
+        addUserShare(config, transactionBlock, {
+            users: slice.map((user) => user[0]),
+            shares: slice.map((user) => user[1]),
+        });
+
+        let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
+        console.log(res);
+        await sleep(5000);
+    }
+})();
