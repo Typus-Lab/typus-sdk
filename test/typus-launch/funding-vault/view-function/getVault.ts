4ac1284490eaa8f7da2db05790a450780708107c
jericlong <jeric.l@alphabit.software>
2024-11-12 14:05:42 +0800

    add funding-vault view-function getFund

--- test/typus-launch/funding-vault/view-function/getVault.ts
+++ test/typus-launch/funding-vault/view-function/getVault.ts
@@ -3,6 +3,6 @@ import { TypusConfig } from "src/utils";
 
 (async () => {
     let config = await TypusConfig.default("TESTNET", null);
-    let vaults = await getVault(config, { indexes: ["0", "0"] });
+    let vaults = await getVault(config, { indexes: ["0"] });
     console.log(JSON.stringify(vaults, null, 2));
 })();
