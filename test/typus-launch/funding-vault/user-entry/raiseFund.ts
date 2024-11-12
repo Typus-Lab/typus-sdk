d81a76b809470d246344405471b38c6f7fed8769
jericlong <jeric.l@alphabit.software>
2024-11-12 13:52:40 +0800

    test funding-vault raiseFund

---
+++ test/typus-launch/funding-vault/user-entry/raiseFund.ts
@@ -0,0 +1,24 @@
+import "src/utils/load_env";
+import { raiseFund } from "src/typus-launch/funding-vault";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { SuiClient } from "@mysten/sui.js/client";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { TypusConfig } from "src/utils";
+
+(async () => {
+    let config = await TypusConfig.default("TESTNET", null);
+    let signer = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
+    let provider = new SuiClient({ url: config.rpcEndpoint });
+    let user = signer.toSuiAddress();
+
+    let transactionBlock = new TransactionBlock();
+
+    raiseFund(config, transactionBlock, {
+        typeArguments: ["0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI"],
+        index: "0",
+        coins: [],
+        amount: "1000000000",
+    });
+    let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
+    console.log(res);
+})();
