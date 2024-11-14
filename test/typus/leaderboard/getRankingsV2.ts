e6427d4f70cd7a857a498a6bad4d61c49335e67b
Wayne Kuo <wayne.k@typuslab.com>
2024-11-14 14:02:52 +0800

    Update getRankingsV2.ts

--- test/typus/leaderboard/getRankingsV2.ts
+++ test/typus/leaderboard/getRankingsV2.ts
@@ -5,17 +5,6 @@ import * as fs from "fs";
 import { SuiClient } from "@mysten/sui.js/client";
 
 (async () => {
-    let config = await TypusConfig.default("MAINNET", null);
-    let provider = new SuiClient({ url: config.rpcEndpoint });
-
-    var parentId = "0x8d2b077d51c2b40ab1a8d59f12e29a3c80fdccebcf8e4ef90fe857c197b0cfcc";
-    var filename = "depositor_program.csv";
-    getRankingsV2(provider, parentId, filename);
-
-    var parentId = "0xcbc8ba339173e5696e0194d1ef00d843fad9f5fe0c6c7cbf48df38f88a1fc10c";
-    var filename = "bidding_leaderboard.csv";
-    getRankingsV2(provider, parentId, filename);
-
     let response = await fetch(
         "https://api.sm.xyz/v1/collections/holders/0x034c162f6b594cb5a1805264dd01ca5d80ce3eca6522e6ee37fd9ebfb9d3ddca::factory::PrimeMachin/",
         {
@@ -25,16 +14,44 @@ import { SuiClient } from "@mysten/sui.js/client";
     let response_json = await response.json();
     // @ts-ignore
     let response_array: [string, string][] = Object.entries(response_json["data"]).map(([key, value]) => [key, value.toString()]);
-    saveToFile(response_array, "prime_machin_leaderboard.csv");
+    saveToFile(["user", "num"], response_array, "prime_machin_leaderboard.csv");
+    const sm_holders = response_array.map((item) => item[0]);
+
+    let config = await TypusConfig.default("MAINNET", null);
+    let provider = new SuiClient({ url: config.rpcEndpoint });
+
+    var parentId = "0x8d2b077d51c2b40ab1a8d59f12e29a3c80fdccebcf8e4ef90fe857c197b0cfcc";
+    var filename = "depositor_program.csv";
+    var datas = await getRankingsV2(provider, parentId, filename);
+
+    var new_datas = datas.map((data) => [
+        data[0],
+        sm_holders.includes(data[0]),
+        data[1],
+        sm_holders.includes(data[0]) ? Number(data[1]) * 1.2 : data[1],
+    ]);
+
+    saveToFile(["user", "sm_holder", "score", "final_score"], new_datas, filename);
+
+    var parentId = "0xcbc8ba339173e5696e0194d1ef00d843fad9f5fe0c6c7cbf48df38f88a1fc10c";
+    var filename = "bidding_leaderboard.csv";
+    var datas = await getRankingsV2(provider, parentId, filename);
+
+    var new_datas = datas.map((data) => [
+        data[0],
+        sm_holders.includes(data[0]),
+        data[1],
+        sm_holders.includes(data[0]) ? Number(data[1]) * 1.2 : data[1],
+    ]);
+
+    saveToFile(["user", "sm_holder", "score", "final_score"], new_datas, filename);
 })();
 
-function saveToFile(datas: [string, string][], filename: string) {
-    const headers = ["user", "score"];
-
+function saveToFile(headers: string[], datas: any[][], filename: string) {
     const csvRows = [
         headers.join(","),
-        ...datas.map((leader) => {
-            return `${leader[0]},${leader[1]}`;
+        ...datas.map((data) => {
+            return data.join(",");
         }),
     ];
 
@@ -42,7 +59,7 @@ function saveToFile(datas: [string, string][], filename: string) {
     fs.writeFileSync(filename, csvContent);
 }
 
-async function getRankingsV2(provider: SuiClient, parentId: string, filename: string) {
+async function getRankingsV2(provider: SuiClient, parentId: string, filename: string): Promise<[string, string][]> {
     var result = await provider.getDynamicFields({
         parentId,
     });
@@ -70,5 +87,5 @@ async function getRankingsV2(provider: SuiClient, parentId: string, filename: st
         datas = datas.concat(objects.map((item) => [item.data?.content.fields.name, item.data?.content.fields.value]));
     }
 
-    saveToFile(datas, filename);
+    return datas;
 }
