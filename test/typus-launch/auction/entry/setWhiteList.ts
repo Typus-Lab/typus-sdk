42917e3e5fd4c23c5fea5590b1d45452514d4e28
Wayne Kuo <wayne.k@typuslab.com>
2024-11-18 17:06:17 +0800

    Update setWhiteList.ts

--- test/typus-launch/auction/entry/setWhiteList.ts
+++ test/typus-launch/auction/entry/setWhiteList.ts
@@ -8,12 +8,21 @@ import mnemonic from "mnemonic.json";
 import * as fs from "fs";
 
 (async () => {
-    let config = await TypusConfig.default("TESTNET", null);
-    let signer = Ed25519Keypair.deriveKeypair(String(mnemonic.MNEMONIC));
+    let config = await TypusConfig.default("MAINNET", null);
+    let signer = Ed25519Keypair.deriveKeypair(String(mnemonic.TGE_AUCTION));
     let provider = new SuiClient({ url: config.rpcEndpoint });
 
     const raw = fs.readFileSync("whitelist.csv", "utf-8");
     const user_data = raw.split("\n").map((line) => line.split(","));
+    const addresses = user_data.map((user) => user[0]);
+
+    const raw1 = fs.readFileSync("whitelist1.csv", "utf-8");
+    const user_data1 = raw1.split("\n").map((line) => line.split(","));
+    const addresses1 = user_data1.map((user) => user[0]);
+
+    const duplicated = addresses.filter((x) => addresses1.includes(x));
+    console.log(duplicated);
+
     const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
     while (user_data.length > 0) {
@@ -23,7 +32,7 @@ import * as fs from "fs";
             config,
             new TransactionBlock(),
             slice.map((user) => user[0]),
-            slice.map((user) => user[1])
+            slice.map((user) => (Number(user[1]) * 1000000000).toString())
         );
 
         let res = await provider.signAndExecuteTransactionBlock({ signer, transactionBlock });
