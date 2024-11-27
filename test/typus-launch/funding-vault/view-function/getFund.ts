7699d00680ce069f710faeee64d584f23f5dd84d
Wayne Kuo <wayne.k@typuslab.com>
2024-11-27 17:12:59 +0800

    update getFund

--- test/typus-launch/funding-vault/view-function/getFund.ts
+++ test/typus-launch/funding-vault/view-function/getFund.ts
@@ -3,6 +3,12 @@ import { TypusConfig } from "src/utils";
 
 (async () => {
     let config = await TypusConfig.default("MAINNET", null);
-    let vaults = await getFund(config, { indexes: ["0"], user: "0x603272d1e61de086a18751482684f882642eb080144aee425c4c34b5e56470b0" });
-    console.log(JSON.stringify(vaults, null, 2));
+    let vaults = await getFund(config, { indexes: ["0"], user: "0xd15f079d5f60b8fdfdcf3ca66c0d3473790c758b04b6418929d5d2991c5443ee" });
+    // console.log(JSON.stringify(vaults, null, 2));
+
+    const deposit = vaults["0"].reduce((acc, cur) => {
+        acc += Number(cur.balance) / 1000000000;
+        return acc;
+    }, 0);
+    console.log(`19JAN25 All Weather: ${deposit} SUI`);
 })();
