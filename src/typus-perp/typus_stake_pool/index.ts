db793a5f5ee2074de57ceb4d19ff2ccea1794d4d
Wayne Kuo <wayne.k@typuslab.com>
2024-12-19 17:52:00 +0800

    fix const

--- src/typus-perp/typus_stake_pool/index.ts
+++ src/typus-perp/typus_stake_pool/index.ts
@@ -1,10 +1,3 @@
-// export const PACKAGE_ID = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";
-// export const PUBLISHED_AT = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";
+export const PACKAGE_ID = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";
+export const PUBLISHED_AT = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";
 export const PKG_V1 = "0xe4b92f096c1b6fdcd41e576ed7716ff5c3985aa45b9ef17191de0b3ca2746a35";
-
-import { TypusConfig } from "src/utils";
-import { NETWORK } from "..";
-
-export const PUBLISHED_AT = async () => {
-    return (await TypusConfig.default(NETWORK, null)).package.perp.stakePool;
-};
