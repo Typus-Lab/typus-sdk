32df813191a338bb35a6c0d2b60d05e3fdf972e3
Wayne Kuo <wayne.k@typuslab.com>
2024-12-20 17:48:44 +0800

    update test

--- src/constants/token.ts
+++ src/constants/token.ts
@@ -129,6 +129,7 @@ export const tokenType = {
         HIPPO: "0x8993129d72e733985f7f1a00396cbd055bad6f817fee36576ce483c8bbb8b87b::sudeng::SUDENG",
         MLIQ: "9614657c9d7e8799be4f49781ef1a9247b83ee178976df6c92d29b6026b9dadf::mliq::MLIQ",
         BLUE: "0xe1b45a0e641b9955a20aa0ad1c1f4ad86aad8afb07296d4085e349a50e90bdca::blue::BLUE",
+        TYPUS: "0xf82dc05634970553615eef6112a1ac4fb7bf10272bf6cbe0f80ef44a6c489385::typus::TYPUS",
     },
     TESTNET: {
         SUI: "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI",
@@ -152,6 +153,7 @@ export const tokenType = {
         USDC: "0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC",
         BLUB: "0x94b8f6dcceeb2be160f6837089cc7502458f4f070fa0814380737acb0c41fd5b::blub::BLUB",
         MBLUB: "0x4b5d0a097ee8a309a89bb2bc589403a4a9a39de639d576495b697be2a60f69bb::mblub::MBLUB",
+        TYPUS: "0xaded0918624ba1a31a9818ae73ccb557d46f35cb0d754b34597356ce38e6004d::typus::TYPUS",
     },
 };
 
@@ -181,7 +183,8 @@ export type TOKEN =
     | "HIPPO"
     | "DEEP"
     | "BLUE"
-    | "wUSDC";
+    | "wUSDC"
+    | "TYPUS";
 
 export function typeArgToToken(typeArg: string): string {
     // console.log(typeArg);
