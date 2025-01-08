714d6fc79963ad59131be294b51e216c27d43944
jericlong <jeric.l@alphabit.software>
2025-01-08 16:00:45 +0800

    getReduceFundTx

--- test/typus-dov-single-v2/view-function/getVaults.ts
+++ test/typus-dov-single-v2/view-function/getVaults.ts
@@ -2,9 +2,9 @@ import { TypusConfig } from "src/utils";
 import { getVaults } from "src/typus-dov-single-v2";
 
 (async () => {
-    let config = await TypusConfig.default("TESTNET", null);
+    let config = await TypusConfig.default("MAINNET", null);
 
-    let indexes = [];
+    let indexes = ["70"];
     let result = await getVaults(config, { indexes });
     console.log(JSON.stringify(result, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2));
 })();
