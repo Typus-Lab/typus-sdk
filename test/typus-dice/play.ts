46030359b5e5a55e84a3468b3e7e44e4d0f38610
Wayne Kuo <wayne.k@typuslab.com>
2025-02-14 20:27:18 +0800

    Update play.ts

--- test/typus-dice/play.ts
+++ test/typus-dice/play.ts
@@ -1,6 +1,6 @@
 import "src/utils/load_env";
 import { TypusConfig } from "src/utils";
-import { DrawEvent, getPlaygrounds, newGamePlayGuessTx } from "src/dice";
+import { DrawEvent, getPlaygrounds, playTx } from "src/dice";
 import { SuiClient } from "@mysten/sui/client";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import { Transaction } from "@mysten/sui/transactions";
@@ -9,14 +9,14 @@ import mnemonic from "mnemonic.json";
 const keypair = Ed25519Keypair.deriveKeypair(String(mnemonic.MNEMONIC));
 
 (async () => {
-    let network: "MAINNET" | "TESTNET" = "TESTNET";
+    let network: "MAINNET" | "TESTNET" = "MAINNET";
     let module: "combo_dice" | "tails_exp" = "combo_dice";
     let config = await TypusConfig.default(network, null);
     let provider = new SuiClient({ url: config.rpcEndpoint });
     let user = keypair.toSuiAddress();
 
-    let index = "1";
-    let amount = "100000000000";
+    let index = "0";
+    let amount = "1000000000";
     let guess_1 = "5000";
     let larger_than_1 = true;
     let guess_2 = "5000";
@@ -28,7 +28,7 @@ const keypair = Ed25519Keypair.deriveKeypair(String(mnemonic.MNEMONIC));
     let coinType = "0x" + playground.stake_token;
     let coins = (await provider.getCoins({ owner: user, coinType })).data.map((coin) => coin.coinObjectId);
 
-    let transaction = await newGamePlayGuessTx(config, new Transaction(), {
+    let transaction = await playTx(config, new Transaction(), {
         module,
         typeArguments: [coinType],
         index,
