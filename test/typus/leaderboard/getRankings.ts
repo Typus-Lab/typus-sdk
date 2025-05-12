580951691ea75f844e27e5796b1f750c7668f286
Wayne Kuo <wayne.k@typuslab.com>
2025-05-12 10:53:16 +0800

    Update getRankings.ts

--- test/typus/leaderboard/getRankings.ts
+++ test/typus/leaderboard/getRankings.ts
@@ -6,26 +6,28 @@ import * as fs from "fs";
 (async () => {
     let config = await TypusConfig.default("MAINNET", null);
 
-    const bidding_leaderboard = await getRankings(config, {
-        key: "bidding_leaderboard",
-        id: "0x8a40d11311beee02be5ade3fc7077aefba80e4b92a02823688596cf4d0d10895",
+    const trading_competition = await getRankings(config, {
+        key: "trading_competition",
+        id: "0x805f9effaac3990e31831d26ad629f565a45215ae04bbdce6d7c6627783a10bd", // 5/12 0x571a9134c5fe68731a7955a63dfb453d287a130fa1d5beb8b6313a7df13faa04
         ranks: 300,
-        user: "0x0000000000000000000000000000000000000000000000000000000000000123",
+        user: "0x51f7f4abb6a4cf83ed59ce634f7c17dc1df2f04abe6842317049a3df0f8be8e4",
         active: true,
     });
-    console.log(bidding_leaderboard.leaderboard.length);
-    saveToFile(bidding_leaderboard, "bidding_leaderboard.csv");
+    console.log(JSON.stringify(trading_competition, (_, v) => (typeof v === "bigint" ? `${v}` : v), 2));
 
-    const depositor_program = await getRankings(config, {
-        key: "depositor_program",
-        id: "0x9b145a4d3f5ddb4594201c7df97ea9e02ef2fd77dfff53fc9a45dfde7cfb3543",
-        ranks: 300,
-        user: "0x0000000000000000000000000000000000000000000000000000000000000123",
-        active: true,
-    });
+    // console.log(trading_competition.leaderboard.length);
+    // saveToFile(trading_competition, "trading_competition.csv");
 
-    console.log(depositor_program.leaderboard.length);
-    saveToFile(depositor_program, "depositor_program.csv");
+    // const depositor_program = await getRankings(config, {
+    //     key: "depositor_program",
+    //     id: "0x9b145a4d3f5ddb4594201c7df97ea9e02ef2fd77dfff53fc9a45dfde7cfb3543",
+    //     ranks: 300,
+    //     user: "0x0000000000000000000000000000000000000000000000000000000000000123",
+    //     active: true,
+    // });
+
+    // console.log(depositor_program.leaderboard.length);
+    // saveToFile(depositor_program, "depositor_program.csv");
 })();
 
 function saveToFile(data: Rankings, filename: string) {
