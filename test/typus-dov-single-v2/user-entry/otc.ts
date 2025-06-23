1e28fbb16d68563c9646f5a3811a1e355a4146be
jericlong <jeric.l@alphabit.software>
2025-06-23 15:58:34 +0800

    add otc test

---
+++ test/typus-dov-single-v2/user-entry/otc.ts
@@ -0,0 +1,31 @@
+import "src/utils/load_env";
+import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+import { getVaults } from "src/typus-dov-single-v2";
+import { SuiClient } from "@mysten/sui/client";
+import { Transaction } from "@mysten/sui/transactions";
+import { TypusConfig } from "src/utils";
+import { getUserOtcConfigs, otc } from "src/typus-dov-single-v2/otc-entry";
+
+(async () => {
+    let config = await TypusConfig.default("TESTNET", null);
+    let signer = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
+    let user = signer.toSuiAddress();
+    let provider = new SuiClient({ url: config.rpcEndpoint });
+    let index = "41";
+    let vault = (await getVaults(config, { indexes: [index] }))[index];
+    let otcConfig = (await getUserOtcConfigs(config, { user, indexes: [index] }))[0];
+    let amount = (
+        (BigInt(otcConfig.price) * BigInt(otcConfig.size)) /
+        BigInt(Math.pow(10, Number.parseInt(vault.info.oTokenDecimal)))
+    ).toString();
+    let transaction = new Transaction();
+    otc(config, transaction, {
+        typeArguments: [vault.info.depositToken, vault.info.bidToken],
+        index,
+        coins: (await provider.getCoins({ owner: user, coinType: vault.info.bidToken })).data.map((coin) => coin.coinObjectId),
+        amount,
+    });
+    let res = await provider.signAndExecuteTransaction({ signer, transaction });
+
+    console.log(res);
+})();
