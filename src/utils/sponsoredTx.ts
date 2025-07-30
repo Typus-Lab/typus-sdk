3f91a0e4a76d2788f2d5e84e17330b61a5838974
Wayne Kuo <wayne.k@typuslab.com>
2025-07-30 16:04:13 +0800

    add sponsoredTx

---
+++ src/utils/sponsoredTx.ts
@@ -0,0 +1,41 @@
+import { Transaction } from "@mysten/sui/transactions";
+import { GasStationClient, createSuiClient, buildGaslessTransaction, GaslessTransaction } from "@shinami/clients/sui";
+import { SuiClient } from "@mysten/sui/client";
+
+export async function getSponsoredTx(provider: SuiClient, sender: string, tx: Transaction) {
+    let gaslessTx = await buildGaslessTransaction(tx, {
+        sui: provider,
+        sender,
+    });
+
+    let body = JSON.stringify(gaslessTx);
+
+    let apiUrl = "https://sponsored-gas-752051381467.asia-southeast1.run.app";
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    let response = await fetch(apiUrl, {
+        method: "POST",
+        headers,
+        body,
+    });
+    // console.log(response);
+
+    let sponsoredResponse = (await response.json()) as sponsoredResponse;
+    // console.log(sponsoredResponse);
+
+    return sponsoredResponse;
+
+    // let sponsoredResponse = await gasStationClient.sponsorTransaction(
+    //     await buildGaslessTransaction(gaslessTx, {
+    //         sui: provider,
+    //         sender,
+    //     }) // when gaslessTx.gasBudget is undefined we take advantage of Shinami auto-budgeting
+    // );
+    // console.log(sponsoredResponse);
+}
+
+interface sponsoredResponse {
+    txBytes: string;
+    sponsorSig: string;
+}
---
+++ test/typus-dov-single-v2/user-entry/sponsoredDeposit.ts
@@ -0,0 +1,35 @@
+import "src/utils/load_env";
+import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+import { getRaiseFundTx } from "src/typus-dov-single-v2";
+import { SuiClient } from "@mysten/sui/client";
+import { Transaction } from "@mysten/sui/transactions";
+import { TypusConfig } from "src/utils";
+import { tokenType } from "src/constants";
+import { getSponsoredTx } from "src/utils/sponsoredTx";
+
+(async () => {
+    let config = await TypusConfig.default("MAINNET", null);
+    let signer = Ed25519Keypair.deriveKeypair(String(process.env.MNEMONIC));
+    let sender = signer.toSuiAddress();
+    let provider = new SuiClient({ url: config.rpcEndpoint });
+
+    let transaction = getRaiseFundTx(config, new Transaction(), {
+        typeArguments: [tokenType["MAINNET"].USDC, tokenType["MAINNET"].SUI],
+        index: "76",
+        raiseCoins: (await provider.getCoins({ owner: sender, coinType: tokenType["MAINNET"].USDC })).data.map((coin) => coin.coinObjectId),
+        raiseAmount: "10000000",
+        receipts: [],
+        raiseFromPremium: false,
+        raiseFromInactive: false,
+        user: sender,
+    });
+
+    // For Sponsored Tx
+    let sponsoredResponse = await getSponsoredTx(provider, sender, transaction);
+    let senderSig = await Transaction.from(sponsoredResponse?.txBytes).sign({ signer });
+    let res = await provider.executeTransactionBlock({
+        transactionBlock: sponsoredResponse?.txBytes,
+        signature: [senderSig?.signature, sponsoredResponse?.sponsorSig],
+    });
+    console.log(res);
+})();
