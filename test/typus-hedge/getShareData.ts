409e89df2c1db1e075554faabb2b5c20b5b84b50
Wayne Kuo <wayne.k@typuslab.com>
2025-09-12 14:43:09 +0800

    Update getShareData.ts

--- test/typus-hedge/getShareData.ts
+++ test/typus-hedge/getShareData.ts
@@ -2,13 +2,11 @@ import { getShareData } from "src/typus-hedge";
 import { TypusConfig } from "src/utils";
 
 (async () => {
-    let config = await TypusConfig.default("TESTNET", null);
-    config.package.hedge.hedge = "0xb1ac5bc2e84eb6fb9980c48583db3b8437e79f963e2e38576e4323efe135a9af";
-
-    let user = "0x978f65df8570a075298598a9965c18de9087f9e888eb3430fe20334f5c554cfd";
+    let config = await TypusConfig.default("MAINNET", null);
+    let user = "0x845c22be3e771ac8d90973e9859b5088207527c158f75ba4ac9e6201ca1eedb8";
     let shares = await getShareData(config, {
         user,
-        indexes: [...Array(2).keys()].map((n) => {
+        indexes: [...Array(1).keys()].map((n) => {
             return n.toString();
         }),
     });
