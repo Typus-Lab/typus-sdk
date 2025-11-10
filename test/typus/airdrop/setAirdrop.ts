df889d6e7c008d7a1c56b179c16aac0abb25507e
Wayne Kuo <wayne.k@typuslab.com>
2025-11-10 13:50:17 +0800

    Update setAirdrop.ts

--- test/typus/airdrop/setAirdrop.ts
+++ test/typus/airdrop/setAirdrop.ts
@@ -6,13 +6,12 @@ import { getSetAirdropTx } from "src/typus/airdrop";
 import { TypusConfig } from "src/utils";
 import mnemonic from "mnemonic.json";
 import * as fs from "fs";
-import { TOKEN, tokenType } from "src/constants";
+import { assetToDecimal, TOKEN, tokenType } from "src/constants";
 
 (async () => {
     let network: "MAINNET" | "TESTNET" = "TESTNET";
-    let key = "tlp_compensation_1106";
-    let token: TOKEN = "wUSDC";
-    let index = 2;
+    let tokens: TOKEN[] = ["SUI", "wUSDC", "WBTC", "wETH", "TYPUS"];
+    let file = "tlp_compensation_1";
 
     let config = await TypusConfig.default(network, null);
     let signer = Ed25519Keypair.deriveKeypair(String(mnemonic.MNEMONIC));
@@ -20,41 +19,48 @@ import { TOKEN, tokenType } from "src/constants";
     let address = signer.toSuiAddress();
     console.log(address);
 
-    const raw = fs.readFileSync(`${key}.csv`, "utf-8");
-    const user_data = raw.split("\r\n").map((line) => line.split(","));
+    const raw = fs.readFileSync(`${file}.csv`, "utf-8");
     const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
     // remove headers
-    let header = user_data.splice(0, 1)[0];
-    console.log(header);
-    console.log(user_data[0]);
 
-    let sum = 0;
-    user_data.forEach((x) => (sum += Number(x[index])));
-    console.log(sum);
+    for (let token of tokens) {
+        let user_data = raw.split("\r\n").map((line) => line.split(","));
+        let header = user_data.splice(0, 1)[0];
+        console.log("header:", header);
+        let key = `${file}_${token}`;
+        let index = header.indexOf(token);
+        console.log("index:", index);
+        console.log(user_data[0]);
 
-    while (user_data.length > 0) {
-        console.log(user_data.length);
-        let slice = user_data.splice(0, 300);
+        let sum = 0;
+        user_data.forEach((x) => (sum += Math.round(Number(x[index]) * 10 ** assetToDecimal(token)!)));
+        console.log("sum:", sum);
 
-        let coins = (
-            await provider.getCoins({
-                owner: address,
-                coinType: tokenType[network][token],
-            })
-        ).data.map((coin) => coin.coinObjectId);
+        while (user_data.length > 0) {
+            console.log("users:", user_data.length);
+            let slice = user_data.splice(0, 300);
 
-        let transaction = await getSetAirdropTx(config, new Transaction(), {
-            typeArguments: [tokenType[network][token]],
-            key,
-            coins,
-            amount: sum.toString(),
-            users: slice.map((user) => user[0]),
-            values: slice.map((user) => user[index]),
-        });
+            let coins = (
+                await provider.getCoins({
+                    owner: address,
+                    coinType: tokenType[network][token],
+                })
+            ).data.map((coin) => coin.coinObjectId);
+            console.log("coins:", coins.length);
 
-        let res = await provider.signAndExecuteTransaction({ signer, transaction });
-        console.log(res);
-        await sleep(5000);
+            let transaction = await getSetAirdropTx(config, new Transaction(), {
+                typeArguments: [tokenType[network][token]],
+                key,
+                coins,
+                amount: sum.toString(),
+                users: slice.map((user) => user[0]),
+                values: slice.map((user) => Math.round(Number(user[index]) * 10 ** assetToDecimal(token)!).toString()),
+            });
+
+            let res = await provider.signAndExecuteTransaction({ signer, transaction });
+            console.log(res);
+            await sleep(5000);
+        }
     }
 })();
